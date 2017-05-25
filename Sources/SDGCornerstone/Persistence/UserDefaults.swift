c58bedbfd88e091d45c99498470c47428d52d523
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2017-05-24 21:34:15 -0600

    Rearranged tests for Linux.

--- Sources/SDGCornerstone/Persistence/UserDefaults.swift
+++ Sources/SDGCornerstone/Persistence/UserDefaults.swift
@@ -12,112 +12,106 @@
  See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
  */
 
-import Foundation
+#if !os(Linux)
+    // UserDefaults is inoperable on Linux.
 
-extension UserDefaults {
+    import Foundation
 
-    private static func propertyListValuesAreEqual(_ lhsValue: Any?, _ rhsValue: Any?) -> Bool {
-        if let lhs = lhsValue {
-            if let rhs = rhsValue {
+    extension UserDefaults {
 
-                // All possible property list values are castable to NSObject subclasses and can thereby be checked for equality.
-                guard let lhsObject = lhs as? NSObject else {
-                    preconditionFailure("\(lhs) is not a property list value.")
-                }
-                guard let rhsObject = rhs as? NSObject else {
-                    preconditionFailure("\(rhs) is not a property list value.")
-                }
+        private static func propertyListValuesAreEqual(_ lhsValue: Any?, _ rhsValue: Any?) -> Bool {
+            if let lhs = lhsValue {
+                if let rhs = rhsValue {
 
-                if lhsObject == rhsObject {
-                    return true
+                    // All possible property list values are castable to NSObject subclasses and can thereby be checked for equality.
+                    guard let lhsObject = lhs as? NSObject else {
+                        preconditionFailure("\(lhs) is not a property list value.")
+                    }
+                    guard let rhsObject = rhs as? NSObject else {
+                        preconditionFailure("\(rhs) is not a property list value.")
+                    }
+
+                    if lhsObject == rhsObject {
+                        return true
+                    } else {
+                        return false
+                    }
                 } else {
+                    // Only the right is nil.
                     return false
                 }
             } else {
-                // Only the right is nil.
-                return false
-            }
-        } else {
-            if rhsValue ≠ nil {
-                // Only the left is nil.
-                return false
-            } else {
-                // Both nil
-                return true
-            }
-        }
-    }
-
-    private class ChangeObserver : SharedValueObserver {
-        private static var observers: [UserDefaults: ChangeObserver] = [:]
-        fileprivate static func observer(forDefaults defaults: UserDefaults) -> ChangeObserver {
-            return cached(in: &observers[defaults]) { ChangeObserver(defaults) }
-        }
-        private init(_ defaults: UserDefaults) {
-            self.defaults = defaults
-        }
-        private let defaults: UserDefaults
-        fileprivate func valueChanged(for identifier: String) {
-            let new = UserDefaults.sharedValues[defaults]?[identifier]?.value
-            let existing = defaults.object(forKey: identifier)
-
-            if ¬UserDefaults.propertyListValuesAreEqual(new, existing) {
-                if new ≠ nil {
-                    defaults.set(new, forKey: identifier)
+                if rhsValue ≠ nil {
+                    // Only the left is nil.
+                    return false
                 } else {
-                    defaults.removeObject(forKey: identifier)
+                    // Both nil
+                    return true
                 }
             }
         }
-    }
 
-    #if !os(Linux)
-    private class KVOObserver : NSObject {
-        fileprivate static var observers: [UserDefaults: KVOObserver] = [:]
-        fileprivate static func observer(forDefaults defaults: UserDefaults) -> KVOObserver {
-            return cached(in: &observers[defaults]) { KVOObserver(defaults) }
-        }
-        private init(_ defaults: UserDefaults) {
-            self.defaults = defaults
-        }
-        private let defaults: UserDefaults
-        @objc fileprivate override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey: Any]?, context: UnsafeMutableRawPointer?) {
-
-            guard let key = keyPath else {
-                preconditionFailure("KVO did not report a key path.")
+        private class ChangeObserver : SharedValueObserver {
+            private static var observers: [UserDefaults: ChangeObserver] = [:]
+            fileprivate static func observer(forDefaults defaults: UserDefaults) -> ChangeObserver {
+                return cached(in: &observers[defaults]) { ChangeObserver(defaults) }
             }
+            private init(_ defaults: UserDefaults) {
+                self.defaults = defaults
+            }
+            private let defaults: UserDefaults
+            fileprivate func valueChanged(for identifier: String) {
+                let new = UserDefaults.sharedValues[defaults]?[identifier]?.value
+                let existing = defaults.object(forKey: identifier)
 
-            let new = defaults.object(forKey: key)
-            let existing = UserDefaults.sharedValues[defaults]?[key]?.value
-
-            if ¬UserDefaults.propertyListValuesAreEqual(new, existing) {
-                UserDefaults.sharedValues[defaults]?[key]?.value = defaults.object(forKey: key)
+                if ¬UserDefaults.propertyListValuesAreEqual(new, existing) {
+                    if new ≠ nil {
+                        defaults.set(new, forKey: identifier)
+                    } else {
+                        defaults.removeObject(forKey: identifier)
+                    }
+                }
             }
         }
-    }
-    #endif
 
-    private static var sharedValues: [UserDefaults: [String: Shared<Any?>]] = [:]
-    // [_Workaround: The warning below should be removed once Linux has a means of observing properties. (Swift 3.1.0)_]
-    /// Returns the shared value for the specified key.
-    ///
-    /// - Warning: Modules that may be used on Linux should take note of keys accessed as shared values and never attempt to set those keys by any means other than through the shared value. On Linux, this will cause the shared value to fall out of sync with the value stored by `UserDefaults`, because Linux lacks the Objective‐C runtime and KVO necessary to observe changes made through other APIs.
-    ///
-    /// - Parameters:
-    ///     - key: The key.
-    public func sharedValue(forKey key: String) -> Shared<Any?> {
-        var keys = UserDefaults.sharedValues[self] ?? [:]
-        defer { UserDefaults.sharedValues[self] = keys }
+        private class KVOObserver : NSObject {
+            fileprivate static var observers: [UserDefaults: KVOObserver] = [:]
+            fileprivate static func observer(forDefaults defaults: UserDefaults) -> KVOObserver {
+                return cached(in: &observers[defaults]) { KVOObserver(defaults) }
+            }
+            private init(_ defaults: UserDefaults) {
+                self.defaults = defaults
+            }
+            private let defaults: UserDefaults
+            @objc fileprivate override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey: Any]?, context: UnsafeMutableRawPointer?) {
 
-        let result = cached(in: &keys[key]) {
-            let shared = Shared(object(forKey: key))
-            shared.register(observer: ChangeObserver.observer(forDefaults: self), identifier: key)
-            return shared
+                guard let key = keyPath else {
+                    preconditionFailure("KVO did not report a key path.")
+                }
+
+                let new = defaults.object(forKey: key)
+                let existing = UserDefaults.sharedValues[defaults]?[key]?.value
+
+                if ¬UserDefaults.propertyListValuesAreEqual(new, existing) {
+                    UserDefaults.sharedValues[defaults]?[key]?.value = defaults.object(forKey: key)
+                }
+            }
         }
-        #if !os(Linux)
+
+        private static var sharedValues: [UserDefaults: [String: Shared<Any?>]] = [:]
+        internal func sharedValue(forKey key: String) -> Shared<Any?> {
+            var keys = UserDefaults.sharedValues[self] ?? [:]
+            defer { UserDefaults.sharedValues[self] = keys }
+
+            let result = cached(in: &keys[key]) {
+                let shared = Shared(object(forKey: key))
+                shared.register(observer: ChangeObserver.observer(forDefaults: self), identifier: key)
+                return shared
+            }
             addObserver(KVOObserver.observer(forDefaults: self), forKeyPath: key, options: [.new], context: nil)
-        #endif
 
-        return result
+            return result
+        }
     }
-}
+
+#endif
--- Tests/SDGCornerstoneTests/All Tests/API Tests/PersistenceTests.swift
+++ Tests/SDGCornerstoneTests/All Tests/API Tests/PersistenceTests.swift
@@ -19,42 +19,9 @@ import SDGCornerstone
 
 class PersistenceTests : XCTestCase {
 
-    func testUserDefaults() {
-        let testKey = "SDGTestKey"
-
-        let defaults = UserDefaults.standard
-        defaults.removeObject(forKey: testKey)
-        XCTAssert(defaults.object(forKey: testKey) == nil, "Unexpected value: \(String(describing: defaults.object(forKey: testKey))) ≠ nil")
-
-        let shared = UserDefaults.standard.sharedValue(forKey: testKey)
-        XCTAssert(shared.value == nil, "Unexpected shared value: \(String(describing: shared.value)) ≠ nil")
-
-        #if !os(Linux)
-            // [_Workaround: These should be re‐enabled once Linux has a means of observing properties. (Swift 3.1.0)_]
-
-            defaults.set(true, forKey: testKey)
-            XCTAssert(defaults.bool(forKey: testKey) == true, "Bindings failed: \(defaults.bool(forKey: testKey)) ≠ true")
-            XCTAssert(shared.value as? Bool == true, "Bindings failed: \(String(describing: shared.value as? Bool)) ≠ true")
-
-            defaults.set("A", forKey: testKey)
-            XCTAssert(defaults.string(forKey: testKey) == "A")
-            XCTAssert(shared.value as? String == "A")
-        #endif
-
-        shared.value = 10
-        XCTAssert(shared.value as? Int == 10, "Unexpected shared value: \(String(describing: shared.value)) ≠ 10")
-        XCTAssert(defaults.integer(forKey: testKey) == 10, "Unexpected value: \(String(describing: defaults.object(forKey: testKey))) ≠ 10")
-
-        shared.value = nil
-        XCTAssert(shared.value == nil, "Unexpected shared value: \(String(describing: shared.value)) ≠ nil")
-        XCTAssert(defaults.object(forKey: testKey) == nil, "Unexpected value: \(String(describing: defaults.object(forKey: testKey))) ≠ nil")
-
-        defaults.removeObject(forKey: testKey)
-    }
-
     static var allTests: [(String, (PersistenceTests) -> () throws -> Void)] {
         return [
-            ("testUserDefaults", testUserDefaults)
+            // ("testUserDefaults", testUserDefaults)
         ]
     }
 }
