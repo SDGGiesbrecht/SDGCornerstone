734bff46a0d54800e5c881f95fe6f9601af0b89f
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-01-04 15:05:11 -0600

    Updated copyright dates. (#67)

--- Tests/SDGCornerstoneTests/Example Types/VectorProtocolExample.swift
+++ Tests/SDGCornerstoneTests/Example Types/VectorProtocolExample.swift
@@ -4,7 +4,7 @@
  This source file is part of the SDGCornerstone open source project.
  https://sdggiesbrecht.github.io/SDGCornerstone/SDGCornerstone
 
- Copyright ©2016–2017 Jeremy David Giesbrecht and the SDGCornerstone project contributors.
+ Copyright ©2016–2018 Jeremy David Giesbrecht and the SDGCornerstone project contributors.
 
  Soli Deo gloria.
 
