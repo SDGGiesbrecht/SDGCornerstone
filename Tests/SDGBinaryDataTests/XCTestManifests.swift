56c8020c2ad5a3c44d0938689d77acc24ab1c7ab
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-04-29 16:04:58 -0600

    Refreshed test list.

---
+++ Tests/SDGBinaryDataTests/XCTestManifests.swift
@@ -0,0 +1,32 @@
+/*
+ XCTestManifests.swift
+
+ This source file is part of the SDGCornerstone open source project.
+ https://sdggiesbrecht.github.io/SDGCornerstone/SDGCornerstone
+
+ Copyright ©2018 Jeremy David Giesbrecht and the SDGCornerstone project contributors.
+
+ Soli Deo gloria.
+
+ Licensed under the Apache Licence, Version 2.0.
+ See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
+ */
+
+import XCTest
+
+extension SDGBinaryDataAPITests {
+    static let __allTests = [
+        ("testData", testData),
+        ("testDataStream", testDataStream),
+        ("testLinuxMainGenerationCompatibility", testLinuxMainGenerationCompatibility),
+        ("testUInt", testUInt),
+    ]
+}
+
+#if !os(macOS)
+public func __allTests() -> [XCTestCaseEntry] {
+    return [
+        testCase(SDGBinaryDataAPITests.__allTests),
+    ]
+}
+#endif
