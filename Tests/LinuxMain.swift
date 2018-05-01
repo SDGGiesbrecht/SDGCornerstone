6a88ddb6f337f36ec943e7b356ba253d5e78146b
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-04-30 18:48:33 -0600

    Regenerated test list.

---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,52 @@
+/*
+ LinuxMain.swift
+
+ This source file is part of the SDGCornerstone open source project.
+ https://sdggiesbrecht.github.io/SDGCornerstone/SDGCornerstone
+
+ Copyright ©2018 Jeremy David Giesbrecht and the SDGCornerstone project contributors.
+
+ Soli Deo gloria.
+
+ Licensed under the Apache Licence, Version 2.0.
+ See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
+ */
+
+import XCTest
+
+import SDGCornerstoneDocumentationExampleTests
+import SDGBinaryDataTests
+import SDGCalendarTests
+import SDGCollectionsTests
+import SDGConcurrencyTests
+import SDGControlFlowTests
+import SDGExternalProcessTests
+import SDGGeometryTests
+import SDGLocalizationTests
+import SDGLogicTests
+import SDGMathematicsTests
+import SDGPersistenceTests
+import SDGPrecisionMathematicsTests
+import SDGRandomizationTests
+import SDGTextTests
+import SDGXCTestUtilities
+
+var tests = [XCTestCaseEntry]()
+tests += SDGCornerstoneDocumentationExampleTests.__allTests()
+tests += SDGBinaryDataTests.__allTests()
+tests += SDGCalendarTests.__allTests()
+tests += SDGCollectionsTests.__allTests()
+tests += SDGConcurrencyTests.__allTests()
+tests += SDGControlFlowTests.__allTests()
+tests += SDGExternalProcessTests.__allTests()
+tests += SDGGeometryTests.__allTests()
+tests += SDGLocalizationTests.__allTests()
+tests += SDGLogicTests.__allTests()
+tests += SDGMathematicsTests.__allTests()
+tests += SDGPersistenceTests.__allTests()
+tests += SDGPrecisionMathematicsTests.__allTests()
+tests += SDGRandomizationTests.__allTests()
+tests += SDGTextTests.__allTests()
+tests += SDGXCTestUtilities.__allTests()
+
+XCTMain(tests)
---
+++ Tests/SDGCollectionsTests/XCTestManifests.swift
@@ -0,0 +1,53 @@
+/*
+ XCTestManifests.swift
+
+ This source file is part of the SDGCornerstone open source project.
+ https://sdggiesbrecht.github.io/SDGCornerstone/SDGCornerstone
+
+ Copyright ©2018 Jeremy David Giesbrecht and the SDGCornerstone project contributors.
+
+ Soli Deo gloria.
+
+ Licensed under the Apache Licence, Version 2.0.
+ See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
+ */
+
+import XCTest
+
+extension SDGCollectionsAPITests {
+    static let __allTests = [
+        ("testAbsoluteComplement", testAbsoluteComplement),
+        ("testAlternativePatterns", testAlternativePatterns),
+        ("testArray", testArray),
+        ("testBidirectionalCollection", testBidirectionalCollection),
+        ("testBijectiveMapping", testBijectiveMapping),
+        ("testCollection", testCollection),
+        ("testComparableSet", testComparableSet),
+        ("testCompositePattern", testCompositePattern),
+        ("testConditionalPattern", testConditionalPattern),
+        ("testDictionary", testDictionary),
+        ("testFiniteSet", testFiniteSet),
+        ("testIntensionalSet", testIntensionalSet),
+        ("testIntersection", testIntersection),
+        ("testLinuxMainGenerationCompatibility", testLinuxMainGenerationCompatibility),
+        ("testLiteralPattern", testLiteralPattern),
+        ("testMutableSet", testMutableSet),
+        ("testNotPattern", testNotPattern),
+        ("testPatternClassCluster", testPatternClassCluster),
+        ("testRange", testRange),
+        ("testRangeReplaceableCollection", testRangeReplaceableCollection),
+        ("testRepetitionPattern", testRepetitionPattern),
+        ("testSet", testSet),
+        ("testSetInRepresentableUniverse", testSetInRepresentableUniverse),
+        ("testSymmetricDifference", testSymmetricDifference),
+        ("testUnion", testUnion),
+    ]
+}
+
+#if !os(macOS)
+public func __allTests() -> [XCTestCaseEntry] {
+    return [
+        testCase(SDGCollectionsAPITests.__allTests),
+    ]
+}
+#endif
