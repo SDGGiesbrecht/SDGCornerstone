7bb7b36d7469a0d7a0dd297dd26955e4a102ba28
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-06-21 16:14:51 -0600

    Refreshed.

--- Workspace.swift
+++ Workspace.swift
@@ -1,3 +1,16 @@
+/*
+ Workspace.swift
+
+ This source file is part of the SDGCornerstone open source project.
+ https://sdggiesbrecht.github.io/SDGCornerstone/SDGCornerstone
+
+ Copyright ©2018 Jeremy David Giesbrecht and the SDGCornerstone project contributors.
+
+ Soli Deo gloria.
+
+ Licensed under the Apache Licence, Version 2.0.
+ See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
+ */
 
 import WorkspaceConfiguration
 
@@ -35,11 +48,7 @@ configuration.documentation.readMe.featureList["🇨🇦EN"] = [
     "Use the entire package together by importing the `SDGCornerstone` product, or pick and choose pieces by importing the various component products."
 ].joinedAsLines()
 
-configuration.documentation.readMe.exampleUsage["🇨🇦EN"] = [
-    "## Example Usage",
-    "",
-    "#example(Read‐Me 🇨🇦EN)"
-].joinedAsLines
+configuration.documentation.readMe.exampleUsage["🇨🇦EN"] = "#example(Read‐Me 🇨🇦EN)"
 
 configuration.continuousIntegration.skipSimulatorOutsideContinuousIntegration = true
 configuration.documentation.api.encryptedTravisCIDeploymentKey = "gzx7ARrCgcNJiDtT/FALmdVgEYO5p7ZxlUuzOgwUTe9whOKD18POfAkgtRgYHLT7BMeN6+l9d26FJYfeH9Gvr6M4GVLqFpxKeW/DbcGABiJKok+qXkCXjbW+7ImqqarMyhXLyTZA5CdTAVTMLc9CnpqQJZphih2mbQZf06Jg3ZzCLRcsWmfvoehEgGTkt/xWNomYKZSuXOJZqNAMz847Tdx3rnOz8D41m1y+Y1xEOCdEnxtIg3JYQDs2OGjq0VT61qRaNm3fDf/f/VUK77q6vLUhCAXmdm19Qw5vSRt4u8G6pTuFdHxlRy9NrIHXzFj7IeomvtJzmAgxo+f+zRTgBcwbOpwHy3H2B1DILGwpWxQxsKelSjGJM8mEvs6cdXjTuvuLC4vwrkQyauDFlA2O/O3vZJFGyw6hJT2crVAO6tU2r71I36MgtI7ut8FCuHFVINg9suUY2MxzF1E6sJ3v7Q9btz2HTFpiO/2/v3kSsbt/jJUCv2/dak3TrIlmispW+8Pba/xmQmlPj6MW+LdaWDV6fkexpi7+QyLfPTCAbfPuXx9ePIoWGmrSqe0nDsZIiPC+uPUXVYlj25I84YA+QI3eb2eTVWO/nhw/461184rU6Tv5g2SBj0FIkaDTHe21U8vskKvRTDjwuQ1/uQLl34SoVHPcM+XRl6sb3CNA+Zs="
