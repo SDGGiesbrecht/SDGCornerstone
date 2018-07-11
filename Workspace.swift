5d51dd1e2f194a3ce7b80045820c778442757900
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-07-11 15:58:20 -0600

    Fixed examples.

--- Workspace.swift
+++ Workspace.swift
@@ -48,7 +48,7 @@ configuration.documentation.readMe.featureList["🇨🇦EN"] = [
     "Use the entire package together by importing the `SDGCornerstone` product, or pick and choose pieces by importing the various component products."
 ].joinedAsLines()
 
-configuration.documentation.readMe.exampleUsage["🇨🇦EN"] = "#example(readMe🇨🇦EN)"
+configuration.documentation.readMe.exampleUsage["🇨🇦EN"] = "\u{23}example(readMe🇨🇦EN)"
 
 configuration.continuousIntegration.skipSimulatorOutsideContinuousIntegration = true
 configuration.documentation.api.encryptedTravisCIDeploymentKey = "gzx7ARrCgcNJiDtT/FALmdVgEYO5p7ZxlUuzOgwUTe9whOKD18POfAkgtRgYHLT7BMeN6+l9d26FJYfeH9Gvr6M4GVLqFpxKeW/DbcGABiJKok+qXkCXjbW+7ImqqarMyhXLyTZA5CdTAVTMLc9CnpqQJZphih2mbQZf06Jg3ZzCLRcsWmfvoehEgGTkt/xWNomYKZSuXOJZqNAMz847Tdx3rnOz8D41m1y+Y1xEOCdEnxtIg3JYQDs2OGjq0VT61qRaNm3fDf/f/VUK77q6vLUhCAXmdm19Qw5vSRt4u8G6pTuFdHxlRy9NrIHXzFj7IeomvtJzmAgxo+f+zRTgBcwbOpwHy3H2B1DILGwpWxQxsKelSjGJM8mEvs6cdXjTuvuLC4vwrkQyauDFlA2O/O3vZJFGyw6hJT2crVAO6tU2r71I36MgtI7ut8FCuHFVINg9suUY2MxzF1E6sJ3v7Q9btz2HTFpiO/2/v3kSsbt/jJUCv2/dak3TrIlmispW+8Pba/xmQmlPj6MW+LdaWDV6fkexpi7+QyLfPTCAbfPuXx9ePIoWGmrSqe0nDsZIiPC+uPUXVYlj25I84YA+QI3eb2eTVWO/nhw/461184rU6Tv5g2SBj0FIkaDTHe21U8vskKvRTDjwuQ1/uQLl34SoVHPcM+XRl6sb3CNA+Zs="
