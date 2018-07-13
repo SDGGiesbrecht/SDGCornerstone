82daa3dc5a8f70efaefa49a2bd8baaec16176aaf
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-07-13 15:23:57 -0600

    Added default indices.

--- Sources/SDGBinaryData/UIntBinaryView.swift
+++ Sources/SDGBinaryData/UIntBinaryView.swift
@@ -74,7 +74,12 @@ public struct BinaryView<UIntValue : UIntFamily> : BidirectionalCollection, Coll
     /// The type of the indices of the collection.
     public typealias Index = UIntValue
 
-    #if !swift(>=4.1.50)
+    #if swift(>=4.1.50)
+    // #workaround(Swift 4.1.2, This check can be removed in Swift 4.2)
+    // #documentation(SDGCornerstone.Collection.Indices)
+    /// The type that represents the indices that are valid for subscripting the collection, in ascending order.
+    public typealias Indices = DefaultIndices<BinaryView>
+    #else
     // #workaround(Swift 4.1.2, This section can be removed in Swift 4.2)
     // #documentation(SDGCornerstone.Collection.Indices)
     /// The type that represents the indices that are valid for subscripting the collection, in ascending order.
