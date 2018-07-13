9d4b257f28dff94551c5479df650809c3887d50a
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-07-13 15:26:04 -0600

    Fixed more index references.

--- Sources/SDGText/String Family/LineView.swift
+++ Sources/SDGText/String Family/LineView.swift
@@ -89,9 +89,17 @@ public struct LineView<Base : StringFamily> : BidirectionalCollection, Collectio
 
     // MARK: - Collection
 
+    #if swift(>=4.1.50)
+    // #workaround(Swift 4.1.2, This check can be removed in Swift 4.2)
+    // #documentation(SDGCornerstone.Collection.Indices)
+    /// The type that represents the indices that are valid for subscripting the collection, in ascending order.
+    public typealias Indices = DefaultIndices<LineView>
+    #else
+    // #workaround(Swift 4.1.2, This check can be removed in Swift 4.2)
     // #documentation(SDGCornerstone.Collection.Indices)
     /// The type that represents the indices that are valid for subscripting the collection, in ascending order.
     public typealias Indices = DefaultBidirectionalIndices<LineView>
+    #endif
 
     // #documentation(SDGCornerstone.Collection.startIndex)
     /// The position of the first element in a non‐empty collection.
