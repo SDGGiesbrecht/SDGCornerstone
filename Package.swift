29e71b9d1920e2bf4797a3a5fe1325dd1b4f6ea6
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-08-26 20:25:49 -0600

    Documented products.

--- Package.swift
+++ Package.swift
@@ -46,45 +46,93 @@ let package = Package(
         // #documentation(SDGCornerstone)
         /// A module representing the entire `SDGCornerstone` package. All the other modules can be used from this single import (except test utilities).
         .library(name: "SDGCornerstone", targets: ["SDGCornerstone"]),
+        // #documentation(SDGCornerstoneTestUtilities)
+        /// A module representing the entire set of test utilities. Most of the other test utility modules can be used from this single import.
+        ///
+        /// Nothing in this module depends on `XCTest`, so it can be used anywhere, including on mobile devices in the release configuration.
         .library(name: "SDGCornerstoneTestUtilities", targets: ["SDGCornerstoneTestUtilities"]),
+        // #documentation(SDGXCTestUtilities)
+        /// Additional test utilities which require `XCTest`.
         .library(name: "SDGXCTestUtilities", targets: ["SDGXCTestUtilities"]),
 
         // Individual component modules.
 
+        // #documentation(SDGControlFlow)
+        /// Very low‐level abstractions which can be used to reduce boilerplate code and simplify control flow.
         .library(name: "SDGControlFlow", targets: ["SDGControlFlow"]),
 
+        // #documentation(SDGLogic)
+        /// Extensions to related to Boolean logic.
         .library(name: "SDGLogic", targets: ["SDGLogic"]),
+        // #documentation(SDGLogicTestUtilities)
+        /// Utilities for testing code which uses `SDGLogic`.
         .library(name: "SDGLogicTestUtilities", targets: ["SDGLogicTestUtilities"]),
 
+        // #documentation(SDGMathematics)
+        /// A hierarchy of mathematical protocols and extensions to number types.
         .library(name: "SDGMathematics", targets: ["SDGMathematics"]),
+        // #documentation(SDGMathematicsTestUtilities)
+        /// Utilities for testing code which uses `SDGMathematics`.
         .library(name: "SDGMathematicsTestUtilities", targets: ["SDGMathematicsTestUtilities"]),
 
+        // #documentation(SDGCollections)
+        /// Pattern searching, set logic, and other extensions to collection types.
         .library(name: "SDGCollections", targets: ["SDGCollections"]),
+        // #documentation(SDGCollectionsTestUtilities)
+        /// Utilities for testing code which uses `SDGCollections`.
         .library(name: "SDGCollectionsTestUtilities", targets: ["SDGCollectionsTestUtilities"]),
 
+        // #documentation(SDGBinaryData)
+        /// Extensions related to raw binary data.
         .library(name: "SDGBinaryData", targets: ["SDGBinaryData"]),
 
+        // #documentation(SDGText)
+        /// Extensions related to text and Unicode.
         .library(name: "SDGText", targets: ["SDGText"]),
 
+        // #documentation(SDGPersistence)
+        /// Preferences and simplified file system interactions.
         .library(name: "SDGPersistence", targets: ["SDGPersistence"]),
+        // #documentation(SDGPersistenceTestUtilities)
+        /// Utilities for testing code which uses `SDGPersistence`.
         .library(name: "SDGPersistenceTestUtilities", targets: ["SDGPersistenceTestUtilities"]),
 
+        // #documentation(SDGRandomization)
+        /// Randomization tools.
         .library(name: "SDGRandomization", targets: ["SDGRandomization"]),
+        // #documentation(SDGRandomizationTestUtilities)
+        /// Utilities for testing code which uses `SDGRandomization`.
         .library(name: "SDGRandomizationTestUtilities", targets: ["SDGRandomizationTestUtilities"]),
 
+        // #documentation(SDGLocalization)
+        /// Localization tools and locale information.
         .library(name: "SDGLocalization", targets: ["SDGLocalization"]),
+        // #documentation(SDGLocalizationTestUtilities)
+        /// Utilities for testing code which uses `SDGLocalization`.
         .library(name: "SDGLocalizationTestUtilities", targets: ["SDGLocalizationTestUtilities"]),
 
+        // #documentation(SDGGeometry)
+        /// Extensions related to geometry.
         .library(name: "SDGGeometry", targets: ["SDGGeometry"]),
 
+        // #documentation(SDGCalendar)
+        /// Tools for working with human calendar systems.
         .library(name: "SDGCalendar", targets: ["SDGCalendar"]),
 
+        // #documentation(SDGPrecisionMathematics)
+        /// Arbitrary‐precision number types.
         .library(name: "SDGPrecisionMathematics", targets: ["SDGPrecisionMathematics"]),
 
+        // #documentation(SDGConcurrency)
+        /// Concurrency and threading tools.
         .library(name: "SDGConcurrency", targets: ["SDGConcurrency"]),
 
+        // #documentation(SDGExternalProcess)
+        /// Tools for running external processes and shell commands.
         .library(name: "SDGExternalProcess", targets: ["SDGExternalProcess"]),
 
+        // #documentation(SDGTesting)
+        /// Miscellaneous test utilities.
         .library(name: "SDGTesting", targets: ["SDGTesting"])
     ],
     targets: [
@@ -108,6 +156,10 @@ let package = Package(
             "SDGConcurrency",
             "SDGExternalProcess"
             ]),
+        // @documentation(SDGCornerstoneTestUtilities)
+        /// A module representing the entire set of test utilities. Most of the other test utility modules can be used from this single import.
+        ///
+        /// Nothing in this module depends on `XCTest`, so it can be used anywhere, including on mobile devices in the release configuration.
         .target(name: "SDGCornerstoneTestUtilities", dependencies: [
             "SDGLogicTestUtilities",
             "SDGMathematicsTestUtilities",
@@ -119,6 +171,8 @@ let package = Package(
             "SDGCornerstone",
             "SDGTesting"
             ]),
+        // @documentation(SDGXCTestUtilities)
+        /// Additional test utilities which require `XCTest`.
         .target(name: "SDGXCTestUtilities", dependencies: [
             "SDGTesting",
             "SDGLogic",
@@ -127,15 +181,25 @@ let package = Package(
 
         // Individual component modules.
 
+        // @documentation(SDGControlFlow)
+        /// Very low‐level abstractions which can be used to reduce boilerplate code and simplify control flow.
         .target(name: "SDGControlFlow"),
 
+        // @documentation(SDGLogic)
+        /// Extensions to related to Boolean logic.
         .target(name: "SDGLogic", dependencies: []),
+        // @documentation(SDGLogicTestUtilities)
+        /// Utilities for testing code which uses `SDGLogic`.
         .target(name: "SDGLogicTestUtilities", dependencies: ["SDGLogic", "SDGTesting"]),
 
+        // @documentation(SDGMathematics)
+        /// A hierarchy of mathematical protocols and extensions to number types.
         .target(name: "SDGMathematics", dependencies: [
             "SDGControlFlow",
             "SDGLogic"
             ]),
+        // @documentation(SDGMathematicsTestUtilities)
+        /// Utilities for testing code which uses `SDGMathematics`.
         .target(name: "SDGMathematicsTestUtilities", dependencies: [
             "SDGMathematics", "SDGTesting",
             "SDGLogicTestUtilities",
@@ -143,22 +207,30 @@ let package = Package(
             "SDGPersistenceTestUtilities"
             ]),
 
+        // @documentation(SDGCollections)
+        /// Pattern searching, set logic, and other extensions to collection types.
         .target(name: "SDGCollections", dependencies: [
             "SDGControlFlow",
             "SDGLogic",
             "SDGMathematics"
             ]),
+        // @documentation(SDGCollectionsTestUtilities)
+        /// Utilities for testing code which uses `SDGCollections`.
         .target(name: "SDGCollectionsTestUtilities", dependencies: [
             "SDGCollections", "SDGTesting",
             "SDGLogicTestUtilities"
             ]),
 
+        // @documentation(SDGBinaryData)
+        /// Extensions related to raw binary data.
         .target(name: "SDGBinaryData", dependencies: [
             "SDGControlFlow",
             "SDGMathematics",
             "SDGCollections"
             ]),
 
+        // @documentation(SDGText)
+        /// Extensions related to text and Unicode.
         .target(name: "SDGText", dependencies: [
             "SDGControlFlow",
             "SDGLogic",
@@ -166,12 +238,16 @@ let package = Package(
             "SDGCollections"
             ]),
 
+        // @documentation(SDGPersistence)
+        /// Preferences and simplified file system interactions.
         .target(name: "SDGPersistence", dependencies: [
             "SDGControlFlow",
             "SDGLogic",
             "SDGCollections",
             "SDGText"
             ]),
+        // @documentation(SDGPersistenceTestUtilities)
+        /// Utilities for testing code which uses `SDGPersistence`.
         .target(name: "SDGPersistenceTestUtilities", dependencies: [
             "SDGPersistence", "SDGTesting",
             "SDGControlFlow",
@@ -183,15 +259,21 @@ let package = Package(
             "SDGCornerstoneLocalizations"
             ]),
 
+        // @documentation(SDGRandomization)
+        /// Randomization tools.
         .target(name: "SDGRandomization", dependencies: [
             "SDGControlFlow",
             "SDGLogic",
             "SDGMathematics"
             ]),
+        // @documentation(SDGRandomizationTestUtilities)
+        /// Utilities for testing code which uses `SDGRandomization`.
         .target(name: "SDGRandomizationTestUtilities", dependencies: [
             "SDGRandomization", "SDGTesting"
             ]),
 
+        // @documentation(SDGLocalization)
+        /// Localization tools and locale information.
         .target(name: "SDGLocalization", dependencies: [
             "SDGControlFlow",
             "SDGLogic",
@@ -201,16 +283,22 @@ let package = Package(
             "SDGPersistence",
             "SDGRandomization"
             ]),
+        // @documentation(SDGLocalizationTestUtilities)
+        /// Utilities for testing code which uses `SDGLocalization`.
         .target(name: "SDGLocalizationTestUtilities", dependencies: [
             "SDGLocalization", "SDGTesting",
             "SDGPersistenceTestUtilities"
             ]),
 
+        // @documentation(SDGGeometry)
+        /// Extensions related to geometry.
         .target(name: "SDGGeometry", dependencies: [
             "SDGControlFlow",
             "SDGMathematics"
             ]),
 
+        // @documentation(SDGCalendar)
+        /// Tools for working with human calendar systems.
         .target(name: "SDGCalendar", dependencies: [
             "SDGControlFlow",
             "SDGLogic",
@@ -221,6 +309,8 @@ let package = Package(
             "SDGCornerstoneLocalizations"
             ]),
 
+        // @documentation(SDGPrecisionMathematics)
+        /// Arbitrary‐precision number types.
         .target(name: "SDGPrecisionMathematics", dependencies: [
             "SDGLogic",
             "SDGMathematics",
@@ -231,11 +321,15 @@ let package = Package(
             "SDGCornerstoneLocalizations"
             ]),
 
+        // @documentation(SDGConcurrency)
+        /// Concurrency and threading tools.
         .target(name: "SDGConcurrency", dependencies: [
             "SDGControlFlow",
             "SDGLogic"
             ]),
 
+        // @documentation(SDGExternalProcess)
+        /// Tools for running external processes and shell commands.
         .target(name: "SDGExternalProcess", dependencies: [
             "SDGControlFlow",
             "SDGLogic",
@@ -243,6 +337,8 @@ let package = Package(
             "SDGLocalization"
             ]),
 
+        // @documentation(SDGTesting)
+        /// Miscellaneous test utilities.
         .target(name: "SDGTesting", dependencies: [
             "SDGControlFlow",
             "SDGMathematics",
