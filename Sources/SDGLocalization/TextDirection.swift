e0d60bbedcd81f3c14a5abcbc788b6e4b3e161f8
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-09-05 19:56:32 -0600

    Fixed style.

--- Sources/SDGLocalization/TextDirection.swift
+++ Sources/SDGLocalization/TextDirection.swift
@@ -15,7 +15,7 @@
 import SDGControlFlow
 
 /// A direction in which natural text is written.
-public enum TextDirection: Int, IterableEnumeration {
+public enum TextDirection : Int, IterableEnumeration {
 
     /// Written right‐to‐left, top‐to‐bottom, such as Hebrew, Arabic or Persian.
     case rightToLeftTopToBottom
