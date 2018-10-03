9d940583bc153d9e23eab1db050060c54a5b2efd
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-10-03 19:15:56 +0300

    Updated workarounds.

--- Sources/SDGMathematics/Numeric_Protocols/RealArithmetic.swift
+++ Sources/SDGMathematics/Numeric_Protocols/RealArithmetic.swift
@@ -241,7 +241,7 @@ public protocol RealArithmetic : RationalArithmetic {
 
 extension RealArithmetic {
 
-    // #workaround(Swift 4.1.2, These can be removed when global generic constants are available.)
+    // #workaround(Swift 4.2, These can be removed when global generic constants are available.)
 
     /// π in the same type.
     ///
@@ -500,7 +500,7 @@ extension RealArithmetic {
 
 // MARK: - Real Arithmetic
 
-// #workaround(Swift 4.1.2, These should be switched to generic constants when they become available.)
+// #workaround(Swift 4.2, These should be switched to generic constants when they become available.)
 
 /// An instance of π in the desired return type.
 ///
