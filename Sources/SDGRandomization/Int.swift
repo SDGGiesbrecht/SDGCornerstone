3c85f5a48926f0c4f307595fbdbd8dca83e3c187
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-10-05 22:02:09 +0300

    Updated Int family.

--- Sources/SDGRandomization/Int.swift
+++ Sources/SDGRandomization/Int.swift
@@ -12,47 +12,6 @@
  See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
  */
 
-import SDGMathematics
-
-extension IntFamily {
-
-    // #documentation(SDGCornerstone.WholeArithmetic.init(randomInRange:fromRandomizer:))
-    /// Creates a random value within a particular range using the specified randomizer.
-    ///
-    /// - Parameters:
-    ///     - range: The allowed range for the random value.
-    ///     - randomizer: The randomizer to use to generate the random value.
-    public init(randomInRange range: ClosedRange<Self>, fromRandomizer randomizer: Randomizer) {
-        let value = IntMax(randomInRange: IntMax(range.lowerBound) ... IntMax(range.upperBound), fromRandomizer: randomizer)
-        self.init(value)
-    }
-}
-
-extension SDGMathematics.IntMax {
-
-    // MARK: - RandomizableNumber
-
-    // #documentation(SDGCornerstone.WholeArithmetic.init(randomInRange:fromRandomizer:))
-    /// Creates a random value within a particular range using the specified randomizer.
-    ///
-    /// - Parameters:
-    ///     - range: The allowed range for the random value.
-    ///     - randomizer: The randomizer to use to generate the random value.
-    public init(randomInRange range: ClosedRange<Int64>, fromRandomizer randomizer: Randomizer) {
-
-        if range.lowerBound.isWhole {
-            let unsignedRange: ClosedRange<UInt64> = UInt64(range.lowerBound) ... UInt64(range.upperBound)
-            let unsigned = UInt64(randomInRange: unsignedRange, fromRandomizer: randomizer)
-            self = Int64(unsigned)
-        } else {
-            let span = range.upperBound − range.lowerBound
-            let unsignedRange: ClosedRange<UInt64> = 0 ... UInt64(span)
-            let unsigned = UInt64(randomInRange: unsignedRange, fromRandomizer: randomizer)
-            self = range.lowerBound + Int64(unsigned)
-        }
-    }
-}
-
 extension Int : RandomizableNumber {}
 extension Int64 : RandomizableNumber {}
 extension Int32 : RandomizableNumber {}
