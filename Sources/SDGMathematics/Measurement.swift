c6de3c34e574a7d47539e35819717340e0016599
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-10-05 23:37:44 +0300

    Sank measurement.

--- Sources/SDGMathematics/Measurement.swift
+++ Sources/SDGMathematics/Measurement.swift
@@ -346,6 +346,56 @@ extension Measurement {
         return nonmutatingVariant(of: { $0.round($1, toMultipleOf: $2) }, on: self, with: (rule, factor))
     }
 
+    // @documentation(SDGCornerstone.Measurement.init(randomInRange:))
+    /// Returns a random value within a particular range.
+    ///
+    /// - Precondition: `range` is not empty.
+    ///
+    /// - Parameters:
+    ///     - range: The allowed range for the random value.
+    @inlinable public static func random(in range: Range<Self>) -> Self {
+        var generator = SystemRandomNumberGenerator()
+        return random(in: range, using: &generator)
+    }
+
+    // #documentation(SDGCornerstone.Measurement.init(randomInRange:))
+    /// Creates a random value within a particular range.
+    ///
+    /// - Precondition: `range` is not empty.
+    ///
+    /// - Parameters:
+    ///     - range: The allowed range for the random value.
+    @inlinable public static func random(in range: ClosedRange<Self>) -> Self {
+        var generator = SystemRandomNumberGenerator()
+        return random(in: range, using: &generator)
+    }
+
+    // @documentation(SDGCornerstone.Measurement.init(randomInRange:fromRandomizer:))
+    /// Creates a random value within a particular range using the specified randomizer.
+    ///
+    /// - Precondition: `range` is not empty.
+    ///
+    /// - Parameters:
+    ///     - range: The allowed range for the random value.
+    ///     - generator: The randomizer to use to generate the random value.
+    @inlinable public static func random<R>(in range: Range<Self>, using generator: inout R) -> Self where R : RandomNumberGenerator {
+        let scalar = Scalar.random(in: range.lowerBound.rawValue ..< range.upperBound.rawValue, using: &generator)
+        return Self(rawValue: scalar)
+    }
+
+    // #documentation(SDGCornerstone.Measurement.init(randomInRange:fromRandomizer:))
+    /// Creates a random value within a particular range using the specified randomizer.
+    ///
+    /// - Precondition: `range` is not empty.
+    ///
+    /// - Parameters:
+    ///     - range: The allowed range for the random value.
+    ///     - randomizer: The randomizer to use to generate the random value.
+    @inlinable public static func random<R>(in range: ClosedRange<Self>, using generator: inout R) -> Self where R : RandomNumberGenerator {
+        let scalar = Scalar.random(in: range.lowerBound.rawValue ... range.upperBound.rawValue, using: &generator)
+        return Self(rawValue: scalar)
+    }
+
     // MARK: - Addable
 
     // #documentation(SDGCornerstone.Addable(Summation).+)
