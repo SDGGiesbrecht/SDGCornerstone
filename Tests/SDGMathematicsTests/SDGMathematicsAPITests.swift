a9fb204d86ce58bcdc3697995467e15c846adc6b
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-10-07 15:54:32 +0300

    Added logarithm and trigonometry tests.

--- Tests/SDGMathematicsTests/SDGMathematicsAPITests.swift
+++ Tests/SDGMathematicsTests/SDGMathematicsAPITests.swift
@@ -313,6 +313,9 @@ class SDGMathematicsAPITests : TestCase {
         init(_ value: Double) {
             self.value = value
         }
+        static func == (precedingValue: RealArithmeticExample, followingValue: RealArithmeticExample) -> Bool {
+            return precedingValue.value == followingValue.value
+        }
         static func < (precedingValue: RealArithmeticExample, followingValue: RealArithmeticExample) -> Bool {
             return precedingValue.value < followingValue.value
         }
@@ -375,8 +378,12 @@ class SDGMathematicsAPITests : TestCase {
         XCTAssertEqual(0.τ, Double.τ)
         XCTAssertEqual(e(), Double.e)
         XCTAssert((√RealArithmeticExample(4)).value ≈ RealArithmeticExample(2).value)
+        test(function: (log, "log"), on: 100 as RealArithmeticExample, returns: 2)
         XCTAssert(ln(RealArithmeticExample(714)).value ≈ 6.570_88)
         XCTAssert(cos(RealArithmeticExample(401).radians).value ≈ 0.432_21)
+        XCTAssert(tan((5 as RealArithmeticExample).rad).value ≈ −3.380_52)
+        XCTAssert(arcsin((1 ÷ 6 as RealArithmeticExample)).inRadians.value ≈ 0.167_44)
+        XCTAssert(arccos((1 ÷ 7 as RealArithmeticExample)).inRadians.value ≈ 1.427_44)
     }
 
     func testSequence() {
