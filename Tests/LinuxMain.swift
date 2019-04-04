cf2b71b12937ffb6eb4edeb1fbf3b8d18ffbbd54
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-04-04 11:32:47 -0600

    Updated test manifests.

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,25 +1,11 @@
-/*
- LinuxMain.swift
-
- This source file is part of the SDGCornerstone open source project.
- https://sdggiesbrecht.github.io/SDGCornerstone
-
- Copyright ©2018–2019 Jeremy David Giesbrecht and the SDGCornerstone project contributors.
-
- Soli Deo gloria.
-
- Licensed under the Apache Licence, Version 2.0.
- See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
- */
-
 import XCTest
 
-import SDGCornerstoneDocumentationExampleTests
 import SDGBinaryDataTests
 import SDGCalendarTests
 import SDGCollectionsTests
 import SDGConcurrencyTests
 import SDGControlFlowTests
+import SDGCornerstoneDocumentationExampleTests
 import SDGExternalProcessTests
 import SDGGeometryTests
 import SDGLocalizationTests
@@ -29,15 +15,14 @@ import SDGPersistenceTests
 import SDGPrecisionMathematicsTests
 import SDGRandomizationTests
 import SDGTextTests
-import SDGXCTestUtilities
 
 var tests = [XCTestCaseEntry]()
-tests += SDGCornerstoneDocumentationExampleTests.__allTests()
 tests += SDGBinaryDataTests.__allTests()
 tests += SDGCalendarTests.__allTests()
 tests += SDGCollectionsTests.__allTests()
 tests += SDGConcurrencyTests.__allTests()
 tests += SDGControlFlowTests.__allTests()
+tests += SDGCornerstoneDocumentationExampleTests.__allTests()
 tests += SDGExternalProcessTests.__allTests()
 tests += SDGGeometryTests.__allTests()
 tests += SDGLocalizationTests.__allTests()
