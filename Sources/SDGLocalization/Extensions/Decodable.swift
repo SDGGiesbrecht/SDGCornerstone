fac7b1743c7ab72ca385299a4d5b7a03740dccb3
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-06-10 14:23:25 -0600

    Fixed localization.

--- Sources/SDGLocalization/Extensions/Decodable.swift
+++ Sources/SDGLocalization/Extensions/Decodable.swift
@@ -13,6 +13,7 @@
  */
 
 import SDGControlFlow
+import SDGText
 
 extension Decodable {
 
--- Sources/SDGLocalization/Extensions/Numbers/RationalArithmetic.swift
+++ Sources/SDGLocalization/Extensions/Numbers/RationalArithmetic.swift
@@ -14,6 +14,7 @@
 
 import SDGLogic
 import SDGMathematics
+import SDGText
 
 extension RationalArithmetic {
 
