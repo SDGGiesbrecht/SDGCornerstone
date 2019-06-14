ea6d152b8af1c75cc47d61644a3c7e4058afa56d
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-06-14 17:07:53 -0600

    Updated Workspace.

--- Package.swift
+++ Package.swift
@@ -16,6 +16,7 @@
 
 import PackageDescription
 
+// #example(1, readMe🇨🇦EN)
 /// SDGCornerstone forms the foundation of the SDG module family. It establishes design patterns and provides general‐use extensions to the [Swift Standard Library](https://developer.apple.com/reference/swift) and [Foundation](https://developer.apple.com/reference/foundation).
 ///
 /// > [הִנְנִי יִסַּד בְּצִיּוֹן אָבֶן אֶבֶן בֹּחַן פִּנַּת יִקְרַת מוּסָד מוּסָד׃](https://www.biblegateway.com/passage/?search=Isaiah+28&version=WLC;NIV)
@@ -36,6 +37,105 @@ import PackageDescription
 /// - A simple API for running shell commands on desktop platforms. (`SDGExternalProcess`)
 ///
 /// ...and much more.
+///
+/// ### Exampe Usage
+///
+/// ```swift
+/// // ••••••• Localization •••••••
+///
+/// enum ApplicationLocalization : String, Localization {
+///     case english = "en"
+///     case français = "fr"
+///     static let fallbackLocalization = ApplicationLocalization.english
+/// }
+///
+/// // Define
+/// let text = UserFacing<StrictString, ApplicationLocalization>({ localization in
+///     switch localization {
+///     case .english:
+///         return "Hello, world!"
+///     case .français:
+///         return "Bonjour, le monde !"
+///     }
+/// })
+///
+/// // Use
+/// XCTAssertEqual(text.resolved(),
+///                "Hello, world!")
+///
+/// // ••••••• Preferences •••••••
+///
+/// let preferences = PreferenceSet.applicationPreferences
+///
+/// // Save
+/// preferences["name"].value.set(to: "John Doe")
+/// // Load
+/// let loaded: String? = preferences["name"].value.as(String.self)
+///
+/// XCTAssertEqual(loaded,
+///                "John Doe")
+///
+/// // ••••••• File System •••••••
+///
+/// let url = FileManager.default.url(in: .applicationSupport, at: "folder/file.txt")
+/// do {
+///     // Save
+///     try "Contents".save(to: url)
+///     // Load
+///     let loaded = try String(from: url)
+///
+///     XCTAssertEqual(loaded,
+///                    "Contents")
+/// } catch {
+///     XCTFail(error.localizedDescription)
+/// }
+///
+/// // ••••••• Shared Values •••••••
+///
+/// class Owner {
+///     var property: Shared<String>
+///     init(property: Shared<String>) {
+///         self.property = property
+///     }
+/// }
+///
+/// let originalOwner = Owner(property: Shared("original"))
+/// let anotherOwner = Owner(property: originalOwner.property)
+///
+/// anotherOwner.property.value = "changed"
+/// XCTAssertEqual(originalOwner.property.value,
+///                "changed")
+///
+/// // ••••••• Pattern Matching •••••••
+///
+/// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+/// let pattern = CompositePattern([
+///     LiteralPattern([1]), // 1
+///     ConditionalPattern({ $0.isEven }), // 2
+///     AlternativePatterns([
+///         LiteralPattern([30, 40]), // (∅)
+///         LiteralPattern([3, 4]) // 3, 4
+///         ]),
+///     RepetitionPattern(NotPattern(LiteralPattern([5, 7]))), // 5, 6, 7, 8, 9 (...)
+///     LiteralPattern([10]) // 10
+///     ])
+///
+/// XCTAssertEqual(numbers.firstMatch(for: pattern)?.range,
+///                numbers.startIndex ..< numbers.endIndex)
+///
+/// // ••••••• Arbitrary Precision Arithmetic •••••••
+///
+/// let tenDuotrigintillion: WholeNumber = "10 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000"
+/// XCTAssert(tenDuotrigintillion.isDivisible(by: 10))
+///
+/// #if !(os(iOS) || os(watchOS) || os(tvOS))
+///
+/// // ••••••• Shell Commands •••••••
+///
+/// XCTAssertEqual(try? Shell.default.run(command: ["echo", "Hello, world!"]).get(),
+///                "Hello, world!")
+/// #endif
+/// ```
 let package = Package(
     name: "SDGCornerstone",
     platforms: [
