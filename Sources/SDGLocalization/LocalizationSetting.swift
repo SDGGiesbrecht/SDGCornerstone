d53d88a2f463fc0ff4bf68798abcb25ba966f0bb
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-09-16 21:28:47 -0600

    Moved localization cache.

--- Sources/SDGLocalization/LocalizationSetting.swift
+++ Sources/SDGLocalization/LocalizationSetting.swift
@@ -27,7 +27,6 @@ public struct LocalizationSetting : Decodable, Encodable, Equatable {
     // MARK: - Static Properties
 
     private static let sdgDomainSuffix = ".Language"
-    internal static let languageDomain = PreferenceSet._sdgCornerstoneDomain + sdgDomainSuffix
     #if os(macOS) || os(iOS) || os(watchOS) || os(tvOS)
     private static let osPreferenceKey = "AppleLanguages"
     #endif
@@ -66,7 +65,8 @@ public struct LocalizationSetting : Decodable, Encodable, Equatable {
     }()
 
     private static let sdgSystemWidePreferences: Shared<Preference> = {
-        let preferences = PreferenceSet.preferences(for: LocalizationSetting.languageDomain)[sdgPreferenceKey]
+        let preferences = PreferenceSet.preferences(
+            for: PreferenceSet._sdgCornerstoneDomain + sdgDomainSuffix)[sdgPreferenceKey]
         preferences.register(observer: ChangeObserver.defaultObserver, reportInitialState: false)
         return preferences
     }()
@@ -226,12 +226,12 @@ public struct LocalizationSetting : Decodable, Encodable, Equatable {
     }
 
     private func stabilityCacheURL<L>(for: L.Type) -> URL {
-        var path = "Stable Localizations"
+        var path = "SDGCornerstone/Stable Localizations"
         path += "/"
         path += String(reflecting: L.self)
         path += "/"
         path += orderOfPrecedence.map({ $0.joined(separator: ",") }).joined(separator: ";")
-        return FileManager.default.url(in: .cache, for: LocalizationSetting.languageDomain, at: path)
+        return FileManager.default.url(in: .cache, at: path)
     }
 
     private subscript<L>(stabilityCacheFor type: L.Type) -> CachedLocalization<L>? {
