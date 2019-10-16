33f327f7bb3c59c78d6b77ad9947009c3eba6f1c
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-10-16 17:13:26 -0600

    Added n‐ary variants.

--- Sources/SDGCollections/Ordered/Patterns/AnyPattern.swift
+++ Sources/SDGCollections/Ordered/Patterns/AnyPattern.swift
@@ -1,5 +1,5 @@
 /*
- PatternWrapper.swift
+ AnyPattern.swift
 
  This source file is part of the SDGCornerstone open source project.
  https://sdggiesbrecht.github.io/SDGCornerstone
---
+++ Sources/SDGCollections/Ordered/Patterns/NaryConcatenatedPatterns.swift
@@ -0,0 +1,99 @@
+/*
+ NaryConcatenatedPatterns.swift
+
+ This source file is part of the SDGCornerstone open source project.
+ https://sdggiesbrecht.github.io/SDGCornerstone
+
+ Copyright ©2019 Jeremy David Giesbrecht and the SDGCornerstone project contributors.
+
+ Soli Deo gloria.
+
+ Licensed under the Apache Licence, Version 2.0.
+ See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
+ */
+
+import SDGControlFlow
+
+/// A pattern that matches against several component patterns contiguously.
+public struct NaryConcatenatedPatterns<ComponentPattern> : Pattern, CustomStringConvertible, ExpressibleByArrayLiteral, TextualPlaygroundDisplay
+where ComponentPattern : Pattern {
+
+    // MARK: - Initialization
+
+    /// Creates a repetition pattern from several component patterns.
+    ///
+    /// - Parameters:
+    ///     - components: The component patterns.
+    @inlinable public init(_ components: [ComponentPattern]) {
+        self.components = components
+    }
+
+    // MARK: - Properties
+
+    @usableFromInline internal var components: [ComponentPattern]
+
+    // MARK: - ExpressibleByArrayLiteral
+
+    @inlinable public init(arrayLiteral: ComponentPattern...) {
+        self.init(arrayLiteral)
+    }
+
+    // MARK: - Pattern
+
+    public typealias Element = ComponentPattern.Element
+
+    @inlinable internal func advance<P, C>(
+        ends endIndices: inout [C.Index],
+        for pattern: P,
+        in collection: C)
+        where P : Pattern, C : SearchableCollection, C.Element == Element, P.Element == Element {
+        endIndices = endIndices
+            .lazy.map({ pattern.matches(in: collection, at: $0) })
+            .joined()
+            .map({ $0.upperBound })
+    }
+
+    @inlinable public func matches<C : SearchableCollection>(in collection: C, at location: C.Index) -> [Range<C.Index>] where C.Element == Element {
+
+        var endIndices: [C.Index] = [location]
+        for component in components {
+            if endIndices.isEmpty {
+                // No matches
+                return []
+            } else {
+                // Continue
+                advance(ends: &endIndices, for: component, in: collection)
+            }
+        }
+
+        return endIndices.map { location ..< $0 }
+    }
+
+    @inlinable public func primaryMatch<C : SearchableCollection>(in collection: C, at location: C.Index) -> Range<C.Index>? where C.Element == Element {
+
+        var endIndices: [C.Index] = [location]
+        for component in components {
+            if endIndices.isEmpty {
+                // No matches
+                return nil
+            } else {
+                // Continue
+                advance(ends: &endIndices, for: component, in: collection)
+            }
+        }
+
+        return endIndices.first.map { location ..< $0 }
+    }
+
+    @inlinable public func reversed() -> NaryConcatenatedPatterns<ComponentPattern.Reversed> {
+        return NaryConcatenatedPatterns<ComponentPattern.Reversed>(
+            components.lazy.map({ $0.reversed() }).reversed())
+    }
+
+    // MARK: - CustomStringConvertible
+
+    public var description: String {
+        let entries = components.map { "(" + String(describing: $0) + ")" }
+        return entries.joined(separator: " + ")
+    }
+}
