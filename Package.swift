a4e71feb47421289f4172fa4cf2b066b3621890f
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-12-15 20:41:11 -0600

    Updated workarounds.

--- Package.swift
+++ Package.swift
@@ -728,7 +728,7 @@ let package = Package(
   ]
 )
 
-// #workaround(Swift 5.1.2, The generated Xcode project cannot import XCTest on iOS devices.)
+// #workaround(Swift 5.1.3, The generated Xcode project cannot import XCTest on iOS devices.)
 import Foundation
 let path = ProcessInfo.processInfo.environment["PATH"] ?? ""
 let firstColon = path.range(of: ":")?.lowerBound ?? path.endIndex
