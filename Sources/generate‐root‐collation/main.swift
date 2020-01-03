270d04aa200cca792ca54bcbb4b1eb46b304fbc1
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-01-02 20:31:58 -0600

    Generated.

--- Sources/generate‐root‐collation/main.swift
+++ Sources/generate‐root‐collation/main.swift
@@ -12,5 +12,7 @@
  See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
  */
 
+import SDGCollation
+
 let root = try CollationOrder.generateRoot()
 try root.save(to: collationResourcesDirectory.appendingPathComponent("Root"))
