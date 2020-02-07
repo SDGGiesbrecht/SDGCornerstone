444e9191cf7020a4106cfc52e428ab1ed22e32ce
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-02-06 20:28:13 -0600

    Fixed path separator handling.

--- Sources/SDGPersistence/URL.swift
+++ Sources/SDGPersistence/URL.swift
@@ -22,6 +22,14 @@ public typealias EinheitlicherRessourcenzeiger = URL
 
 extension URL: Comparable {
 
+  private var platformPathSeparator: String {
+    #if os(Windows)
+      return #"\"#
+    #else
+      return "/"
+    #endif
+  }
+
   /// Returns `true` if the URL is in the location described by the specified URL.
   ///
   /// The URL is considered to be inside if it:
@@ -38,10 +46,10 @@ extension URL: Comparable {
       return true
     } else {
       let otherDirectory: String
-      if otherPath.hasSuffix("/") {
+      if otherPath.hasSuffix(platformPathSeparator) {
         otherDirectory = otherPath
       } else {
-        otherDirectory = otherPath + "/"
+        otherDirectory = otherPath + platformPathSeparator
       }
       return path.hasPrefix(otherDirectory)
     }
@@ -59,7 +67,7 @@ extension URL: Comparable {
     }
     let otherLength = other.path.clusters.count
     var relative = path.clusters.dropFirst(otherLength)
-    if relative.first == "/" {
+    if relative.first == platformPathSeparator {
       relative = relative.dropFirst()
     }
     return String(relative)
