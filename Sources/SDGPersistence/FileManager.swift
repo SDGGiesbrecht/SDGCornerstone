d5b83f5fc23a49efeeab635b1211db44fdeb38e4
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-02-13 19:43:00 -0600

    ...

--- Sources/SDGPersistence/FileManager.swift
+++ Sources/SDGPersistence/FileManager.swift
@@ -73,7 +73,7 @@ extension FileManager {
         )
       else {
         #warning("Debugging.")
-        print(location)
+        fatalError("\(location)")
         _unreachable()
       }
       return result
