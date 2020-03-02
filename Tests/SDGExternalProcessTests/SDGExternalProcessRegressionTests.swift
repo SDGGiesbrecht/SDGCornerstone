eb192bb022c6304852babe6e3dada39879479208
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-03-01 21:05:10 -0600

    Revert "Added Android shell test."
    This reverts commit 5334557fd5fab86cbc1b3e8261efaa8c5ed47d17.

--- Tests/SDGExternalProcessTests/SDGExternalProcessRegressionTests.swift
+++ Tests/SDGExternalProcessTests/SDGExternalProcessRegressionTests.swift
@@ -12,7 +12,6 @@
  See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
  */
 
-import SDGLogic
 import SDGExternalProcess
 
 import XCTest
@@ -20,77 +19,6 @@ import XCTest
 import SDGXCTestUtilities
 
 class SDGExternalProcessRegressionTests: TestCase {
-
-  func testAndroidShell() {
-    // Untracked
-
-    #if os(Android)
-      func reportError(_ error: Error, task: String) {
-        let message = [
-          "\nFailed to \(task):",
-          "Error: \(error)",
-          "Localized: \(error.localizedDescription)",
-          "Type: \(type(of: error))",
-        ].joined(separator: "\n")
-        XCTFail(message)
-      }
-
-      let process = Process()
-      process.executableURL = URL(fileURLWithPath: "/system/bin/sh")
-      process.arguments = ["\u{2D}c", "echo \u{27}Hello, world!\u{27}"]
-
-      let pipe = Pipe()
-      process.standardOutput = pipe
-      process.standardError = pipe
-
-      do {
-        try process.run()
-      } catch {
-        reportError(error, task: "run")
-      }
-
-      var output = String()
-      var stream = Data()
-
-      let newLine = "\n"
-      let newLineData = newLine.data(using: String.Encoding.utf8)!
-
-      func read() -> Data? {
-        let new = pipe.fileHandleForReading.availableData
-        return new.isEmpty ? nil : new
-      }
-
-      var end = false
-      while ¬end {
-        guard let newData = read() else {
-          end = true
-          return
-        }
-        stream.append(newData)
-
-        while let lineEnd = stream.range(of: newLineData) {
-          let lineData = stream.subdata(in: (..<lineEnd.lowerBound).relative(to: stream))
-          stream.removeSubrange(..<lineEnd.upperBound)
-
-          guard let line = try? String(file: lineData, origin: nil) else {
-            fatalError("Unable to decode data: \(lineData)")
-          }
-
-          output.append(line + newLine)
-        }
-      }
-
-      while process.isRunning {}
-
-      if output.scalars.last == "\n" {
-        output.scalars.removeLast()
-      }
-
-      let exitCode = process.terminationStatus
-      XCTAssertEqual(exitCode, 0)
-      XCTAssertEqual(output, "Hello, world!")
-    #endif
-  }
 
   func testDelayedShellOutput() throws {
     // Untracked
