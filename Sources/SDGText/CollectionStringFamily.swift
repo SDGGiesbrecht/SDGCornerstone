eb28cacb4f55c205d733b0c9cf3a95929bb8ad56
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-03-06 17:33:07 -0600

    Refreshed.

--- Sources/SDGText/CollectionStringFamily.swift
+++ Sources/SDGText/CollectionStringFamily.swift
@@ -1,5 +1,5 @@
 /*
- Array.swift
+ CollectionStringFamily.swift
 
  This source file is part of the SDGCornerstone open source project.
  https://sdggiesbrecht.github.io/SDGCornerstone
--- Sources/SDGText/CollectionUnicodeScalar.swift
+++ Sources/SDGText/CollectionUnicodeScalar.swift
@@ -1,5 +1,5 @@
 /*
- Collection.swift
+ CollectionUnicodeScalar.swift
 
  This source file is part of the SDGCornerstone open source project.
  https://sdggiesbrecht.github.io/SDGCornerstone
