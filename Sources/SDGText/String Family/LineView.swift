c654cbc5763880c7b908a5b965d4fd9d5373bd23
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-03-13 19:51:33 -0600

    Disabled newline pattern.

--- Sources/SDGText/String Family/LineView.swift
+++ Sources/SDGText/String Family/LineView.swift
@@ -15,158 +15,160 @@
 // #workaround(Swift 5.1.5, Web doesn’t have foundation yet; compiler doesn’t recognize os(WASI).)
 #if canImport(Foundation)
   import Foundation
-#endif
 
-import SDGControlFlow
-import SDGLogic
-import SDGCollections
+  import SDGControlFlow
+  import SDGLogic
+  import SDGCollections
 
-/// A view of a string’s contents as a collection of lines.
-public struct LineView<Base: StringFamily>: BidirectionalCollection, Collection, MutableCollection,
-  RangeReplaceableCollection, TextualPlaygroundDisplay
-{
+  /// A view of a string’s contents as a collection of lines.
+  public struct LineView<Base: StringFamily>: BidirectionalCollection, Collection,
+    MutableCollection,
+    RangeReplaceableCollection, TextualPlaygroundDisplay
+  {
 
-  // MARK: - Initialization
+    // MARK: - Initialization
 
-  @inlinable internal init(_ base: Base) {
-    self.base = base
-    startIndex = Index(start: base.scalars.startIndex)
-  }
-
-  // MARK: - Properties
-
-  @usableFromInline internal var base: Base
-
-  // MARK: - Conversions
-
-  @inlinable internal func line(for scalar: String.ScalarView.Index) -> LineViewIndex {
-    if scalar == base.scalars.endIndex {
-      return endIndex
-    }
-    guard var previousNewline = base.scalars[..<scalar].lastMatch(for: CharacterSet.newlinePattern)
-    else {
-      return startIndex
+    @inlinable internal init(_ base: Base) {
+      self.base = base
+      startIndex = Index(start: base.scalars.startIndex)
     }
 
-    var encounteredNewline: Range<String.ScalarView.Index>?
-    if let newline = CharacterSet.newlinePattern.primaryMatch(
-      in: base.scalars,
-      at: previousNewline.range.lowerBound
-    ),
-      newline.contains(scalar)
-    {
-      // Between CR and LF
+    // MARK: - Properties
 
+    @usableFromInline internal var base: Base
+
+    // MARK: - Conversions
+
+    @inlinable internal func line(for scalar: String.ScalarView.Index) -> LineViewIndex {
+      if scalar == base.scalars.endIndex {
+        return endIndex
+      }
       guard
-        let actualPreviousNewline = base.scalars[..<newline.lowerBound].lastMatch(
-          for: CharacterSet.newlinePattern
-        )
+        var previousNewline = base.scalars[..<scalar].lastMatch(for: CharacterSet.newlinePattern)
       else {
         return startIndex
       }
 
-      previousNewline = actualPreviousNewline
-      encounteredNewline = newline
+      var encounteredNewline: Range<String.ScalarView.Index>?
+      if let newline = CharacterSet.newlinePattern.primaryMatch(
+        in: base.scalars,
+        at: previousNewline.range.lowerBound
+      ),
+        newline.contains(scalar)
+      {
+        // Between CR and LF
+
+        guard
+          let actualPreviousNewline = base.scalars[..<newline.lowerBound].lastMatch(
+            for: CharacterSet.newlinePattern
+          )
+        else {
+          return startIndex
+        }
+
+        previousNewline = actualPreviousNewline
+        encounteredNewline = newline
+      }
+
+      return Index(start: previousNewline.range.upperBound, newline: encounteredNewline)
     }
 
-    return Index(start: previousNewline.range.upperBound, newline: encounteredNewline)
-  }
+    // MARK: - BidirectionalCollection
 
-  // MARK: - BidirectionalCollection
+    @inlinable public func index(before i: LineViewIndex) -> LineViewIndex {
 
-  @inlinable public func index(before i: LineViewIndex) -> LineViewIndex {
+      let newline: Range<String.ScalarView.Index>
+      if i == endIndex {
+        newline = base.scalars.endIndex..<base.scalars.endIndex
+      } else {
+        let searchEnd = i.start ?? base.scalars.endIndex  // @exempt(from: tests)
+        // `nil` ought to have been handled by “if i == endIndex” above.
+        guard
+          let found = base.scalars[..<searchEnd].lastMatch(
+            for: CharacterSet.newlinePattern
+          )?.range
+        else {
+          _preconditionFailure({ (localization: _APILocalization) -> String in
+            switch localization {
+            case .englishCanada:  // @exempt(from: tests)
+              return "No index precedes the start index."
+            }
+          })
+        }
+        newline = found
+      }
 
-    let newline: Range<String.ScalarView.Index>
-    if i == endIndex {
-      newline = base.scalars.endIndex..<base.scalars.endIndex
-    } else {
-      let searchEnd = i.start ?? base.scalars.endIndex  // @exempt(from: tests)
-      // `nil` ought to have been handled by “if i == endIndex” above.
       guard
-        let found = base.scalars[..<searchEnd].lastMatch(
+        let previousNewline = base.scalars[..<newline.lowerBound].lastMatch(
           for: CharacterSet.newlinePattern
         )?.range
       else {
-        _preconditionFailure({ (localization: _APILocalization) -> String in
-          switch localization {
-          case .englishCanada:  // @exempt(from: tests)
-            return "No index precedes the start index."
-          }
-        })
+        startIndex.cache.newline = newline
+        return startIndex
       }
-      newline = found
+      return LineViewIndex(start: previousNewline.upperBound, newline: newline)
     }
 
-    guard
-      let previousNewline = base.scalars[..<newline.lowerBound].lastMatch(
-        for: CharacterSet.newlinePattern
-      )?.range
-    else {
-      startIndex.cache.newline = newline
-      return startIndex
+    // MARK: - Collection
+
+    public typealias Indices = DefaultIndices<LineView>
+
+    public let startIndex: LineViewIndex
+
+    public let endIndex: LineViewIndex = LineViewIndex.endIndex()
+
+    @inlinable public func index(after i: LineViewIndex) -> LineViewIndex {
+      guard let newline = i.newline(in: base.scalars),
+        ¬newline.isEmpty
+      else {
+        return LineViewIndex.endIndex()
+      }
+      return LineViewIndex(start: newline.upperBound)
     }
-    return LineViewIndex(start: previousNewline.upperBound, newline: newline)
-  }
 
-  // MARK: - Collection
-
-  public typealias Indices = DefaultIndices<LineView>
-
-  public let startIndex: LineViewIndex
-
-  public let endIndex: LineViewIndex = LineViewIndex.endIndex()
-
-  @inlinable public func index(after i: LineViewIndex) -> LineViewIndex {
-    guard let newline = i.newline(in: base.scalars),
-      ¬newline.isEmpty
-    else {
-      return LineViewIndex.endIndex()
-    }
-    return LineViewIndex(start: newline.upperBound)
-  }
-
-  @inlinable public subscript(_ position: LineViewIndex) -> Line<Base> {
-    get {
-      let newline = position.newline(in: base.scalars)!
-      let line = base.scalars[position.start!..<newline.lowerBound]
-      return Line(line: line, newline: base.scalars[newline])
-    }
-    set {
-      let replacement = Base.ScalarView(newValue.line) + Base.ScalarView(newValue.newline)
-      if let replacementStart = position.start {
-        base.scalars.replaceSubrange(
-          replacementStart..<position.newline(in: base.scalars)!.upperBound,
-          with: replacement
-        )
-      } else {
-        base.scalars.append(contentsOf: replacement)
+    @inlinable public subscript(_ position: LineViewIndex) -> Line<Base> {
+      get {
+        let newline = position.newline(in: base.scalars)!
+        let line = base.scalars[position.start!..<newline.lowerBound]
+        return Line(line: line, newline: base.scalars[newline])
+      }
+      set {
+        let replacement = Base.ScalarView(newValue.line) + Base.ScalarView(newValue.newline)
+        if let replacementStart = position.start {
+          base.scalars.replaceSubrange(
+            replacementStart..<position.newline(in: base.scalars)!.upperBound,
+            with: replacement
+          )
+        } else {
+          base.scalars.append(contentsOf: replacement)
+        }
       }
     }
-  }
 
-  // MARK: - RangeReplaceableCollection
+    // MARK: - RangeReplaceableCollection
 
-  @inlinable public init() {
-    self.init(Base())
-  }
-
-  @inlinable public mutating func replaceSubrange<S: Sequence>(
-    _ subrange: Range<Index>,
-    with newElements: S
-  ) where S.Element == Line<Base> {
-    var replacement = Base()
-    for line in newElements {
-      replacement.scalars.append(contentsOf: line.line)
-      replacement.scalars.append(contentsOf: line.newline)
+    @inlinable public init() {
+      self.init(Base())
     }
-    let replacementStart = subrange.lowerBound.start ?? base.scalars.endIndex
-    let replacementEnd = subrange.upperBound.start ?? base.scalars.endIndex
-    base.scalars.replaceSubrange(replacementStart..<replacementEnd, with: replacement.scalars)
-  }
 
-  // MARK: - CustomStringConvertible
+    @inlinable public mutating func replaceSubrange<S: Sequence>(
+      _ subrange: Range<Index>,
+      with newElements: S
+    ) where S.Element == Line<Base> {
+      var replacement = Base()
+      for line in newElements {
+        replacement.scalars.append(contentsOf: line.line)
+        replacement.scalars.append(contentsOf: line.newline)
+      }
+      let replacementStart = subrange.lowerBound.start ?? base.scalars.endIndex
+      let replacementEnd = subrange.upperBound.start ?? base.scalars.endIndex
+      base.scalars.replaceSubrange(replacementStart..<replacementEnd, with: replacement.scalars)
+    }
 
-  @inlinable public var description: String {
-    return String(describing: base)
+    // MARK: - CustomStringConvertible
+
+    @inlinable public var description: String {
+      return String(describing: base)
+    }
   }
-}
+#endif
