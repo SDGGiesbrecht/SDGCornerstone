5f565e34b313f0c205c8d43b8c3bcf6b7a836c4c
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-04-02 22:17:31 -0600

    Updated Xcode workaround.

--- Package.swift
+++ Package.swift
@@ -742,7 +742,7 @@ if firstEntry.hasSuffix("/Contents/Developer/usr/bin") {
   sdgXCTestUtilities.swiftSettings = settings
 }
 
-// #workaround(workspace version 0.30.2, Causes Xcode executable/scheme issues for iOS.)
+// #workaround(Swift 5.2.0, The generated Xcode project cannot handle tools when building for iOS.)
 func disableDevelopmentTools() {
   package.targets.removeAll(where: { $0.name == "generate‐root‐collation" })
 }
