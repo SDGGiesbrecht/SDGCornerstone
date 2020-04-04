9de6dbe816ffc202a7ec1fb95da63fe93aaa353b
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-04-03 22:44:11 -0600

    ...

--- Tests/SDGCornerstoneDocumentationExampleTests/ReadMeExampleTests.swift
+++ Tests/SDGCornerstoneDocumentationExampleTests/ReadMeExampleTests.swift
@@ -29,7 +29,7 @@ class ReadMeExampleTests: TestCase {
 
   func testReadMe() {
     #if !os(Windows)  // #workaround(workspace version 0.32.0, SegFault)
-      // #workaround(Swift 5.1.3, Process/Pipe/FileHandle have wires crossed with standard output.)
+      // #workaround(workspace version 0.32.0, Process/Pipe/FileHandle have wires crossed with standard output.)
       #if !os(Android)
         LocalizationSetting(orderOfPrecedence: ["en"]).do {
           // @example(readMe🇨🇦EN)
