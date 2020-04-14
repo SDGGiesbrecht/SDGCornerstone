2d93e3b720dc9cc46e85fffa9fa3c457d18627cf
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-04-14 15:25:03 -0600

    Tried activating test.

--- Tests/SDGBinaryDataTests/APITests.swift
+++ Tests/SDGBinaryDataTests/APITests.swift
@@ -28,7 +28,6 @@ import SDGLocalizationTestUtilities
 class APITests: TestCase {
 
   func testData() {
-    #if !os(Windows)  // #workaround(workspace version 0.32.0, SegFault)
       testBitFieldConformance(
         start: Data([0b0101_0110]),
         not: Data([0b1010_1001]),
@@ -62,7 +61,6 @@ class APITests: TestCase {
         uniqueTestName: "10th",
         overwriteSpecificationInsteadOfFailing: false
       )
-    #endif
   }
 
   func testDataStream() {
