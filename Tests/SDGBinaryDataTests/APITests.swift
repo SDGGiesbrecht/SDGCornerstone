e7d26e70c4ac48a2b5eebf62f8a63bb511cca672
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-04-15 18:54:58 -0600

    ...

--- Tests/SDGBinaryDataTests/APITests.swift
+++ Tests/SDGBinaryDataTests/APITests.swift
@@ -47,10 +47,10 @@ class APITests: TestCase {
       toReverse.binary.reverse()
       XCTAssertEqual(toReverse, Data([0b000000000, 0b00001111]))
 
+    #if !os(Windows)  // #workaround(workspace version 0.32.0, SegFault)
       let alternating = Data([0b01010101, 0b01010101])
       let sorted = Data([0b00000000, 0b11111111])
 
-    #if !os(Windows)  // #workaround(workspace version 0.32.0, SegFault)
       XCTAssertEqual(alternating.bitwiseNot(), Data([0b10101010, 0b10101010]))
       XCTAssertEqual(alternating.bitwiseAnd(with: sorted), Data([0b00000000, 0b01010101]))
       XCTAssertEqual(alternating.bitwiseOr(with: sorted), Data([0b01010101, 0b11111111]))
