7890edcc1163d1fd9548ff67c4912339cfd1df41
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-04-16 21:19:15 -0600

    Updated Workspace.

--- Tests/SDGBinaryDataTests/APITests.swift
+++ Tests/SDGBinaryDataTests/APITests.swift
@@ -28,33 +28,37 @@ import SDGLocalizationTestUtilities
 class APITests: TestCase {
 
   func testData() {
-    #if !os(Windows)  // #workaround(workspace version 0.32.0, SegFault)
-      testBitFieldConformance(
-        start: Data([0b0101_0110]),
-        not: Data([0b1010_1001]),
-        other: Data([0b1101_0010]),
-        and: Data([0b0101_0010]),
-        or: Data([0b1101_0110]),
-        exclusiveOr: Data([0b1000_0100])
-      )
+    testBitFieldConformance(
+      start: Data([0b0101_0110]),
+      not: Data([0b1010_1001]),
+      other: Data([0b1101_0010]),
+      and: Data([0b0101_0010]),
+      or: Data([0b1101_0110]),
+      exclusiveOr: Data([0b1000_0100])
+    )
 
-      let data = Data([UInt8.max])
-      XCTAssertEqual(data.binary.count, 8)
+    let data = Data([UInt8.max])
+    XCTAssertEqual(data.binary.count, 8)
+    #if !os(Windows)  // #workaround(Swift 5.2.2, SegFault)
       XCTAssertEqual(data.binary.map({ $0 ? "1" : "0" }).joined(), "11111111")
+    #endif
 
-      var toReverse = Data([0b11110000, 0b00000000])
+    var toReverse = Data([0b11110000, 0b00000000])
+    #if !os(Windows)  // #workaround(Swift 5.2.2, SegFault)
       toReverse.binary.reverse()
       XCTAssertEqual(toReverse, Data([0b000000000, 0b00001111]))
+    #endif
 
-      let alternating = Data([0b01010101, 0b01010101])
-      let sorted = Data([0b00000000, 0b11111111])
+    let alternating = Data([0b01010101, 0b01010101])
+    let sorted = Data([0b00000000, 0b11111111])
 
-      XCTAssertEqual(alternating.bitwiseNot(), Data([0b10101010, 0b10101010]))
-      XCTAssertEqual(alternating.bitwiseAnd(with: sorted), Data([0b00000000, 0b01010101]))
-      XCTAssertEqual(alternating.bitwiseOr(with: sorted), Data([0b01010101, 0b11111111]))
-      XCTAssertEqual(alternating.bitwiseExclusiveOr(with: sorted), Data([0b01010101, 0b10101010]))
+    XCTAssertEqual(alternating.bitwiseNot(), Data([0b10101010, 0b10101010]))
+    XCTAssertEqual(alternating.bitwiseAnd(with: sorted), Data([0b00000000, 0b01010101]))
+    XCTAssertEqual(alternating.bitwiseOr(with: sorted), Data([0b01010101, 0b11111111]))
+    XCTAssertEqual(alternating.bitwiseExclusiveOr(with: sorted), Data([0b01010101, 0b10101010]))
 
-      var forDescription = Data([0, 0])
+    var forDescription = Data([0, 0])
+    #if !os(Windows)  // #workaround(Swift 5.2.2, SegFault)
       forDescription.binary[11] = true
       testCustomStringConvertibleConformance(
         of: forDescription.binary,
@@ -66,11 +70,11 @@ class APITests: TestCase {
   }
 
   func testDataStream() {
-    #if !os(Windows)  // #workaround(workspace version 0.32.0, SegFault)
-      var inputStream = DataStream()
-      var outputStream = DataStream()
+    var inputStream = DataStream()
+    var outputStream = DataStream()
 
-      var forwards = Data()
+    var forwards = Data()
+    #if !os(Windows)  // #workaround(workspace version 0.32.0, SegFault)
       for byte in (0x00 as Data.Element)...(0xFF as Data.Element) {
         forwards.append(byte)
       }
