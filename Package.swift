97690198be0db95a2eb77aceaedcff591d97f99f
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-04-17 18:15:59 -0600

    Trigger rebuild.

--- Package.swift
+++ Package.swift
@@ -773,4 +773,5 @@ if ProcessInfo.processInfo.environment["TARGETING_WEB"] == "true" {
     // #workaround(Swift 5.2, Web doesn’t have Foundation yet.)
     target.exclude.append("Resources.swift")
   }
+
 }
