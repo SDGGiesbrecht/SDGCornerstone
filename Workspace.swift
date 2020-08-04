7f3189cba8af1d52115f617e8fae56c6fbf82e4b
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-08-03 21:36:13 -0600

    Version 5.4.1

--- Workspace.swift
+++ Workspace.swift
@@ -17,7 +17,7 @@ import WorkspaceConfiguration
 let configuration = WorkspaceConfiguration()
 configuration._applySDGDefaults()
 
-configuration.documentation.currentVersion = Version(5, 4, 0)
+configuration.documentation.currentVersion = Version(5, 4, 1)
 
 configuration.documentation.projectWebsite = URL(
   string: "https://sdggiesbrecht.github.io/SDGCornerstone"
