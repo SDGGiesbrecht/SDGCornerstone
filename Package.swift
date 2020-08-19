861979b9911444f8b338459bd42bb0673bbac83c
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-08-19 16:56:38 -0600

    Switched to Xcode’s native package support.

--- Package.swift
+++ Package.swift
@@ -761,6 +761,11 @@ if ProcessInfo.processInfo.environment["TARGETING_WEB"] == "true" {
   }
 }
 
+if ProcessInfo.processInfo.environment["TARGETING_WATCHOS"] == "true" {
+  // #workaround(xcodebuild -version 11.6, Test targets don’t work on watchOS.) @exempt(from: unicode)
+  package.targets.removeAll(where: { $0.isTest })
+}
+
 // Windows Tests (Generated automatically by Workspace.)
 import Foundation
 if ProcessInfo.processInfo.environment["TARGETING_WINDOWS"] == "true" {
