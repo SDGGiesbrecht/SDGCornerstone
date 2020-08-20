e8cadfe1b162879814e26d80f8d638da1ec56d31
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-08-19 19:30:13 -0600

    Removed Xcode workaround.

--- Sources/SDGXCTestUtilities/TestCase.swift
+++ Sources/SDGXCTestUtilities/TestCase.swift
@@ -16,34 +16,31 @@
 
   // #workaround(Swift 5.2.4, Web doesn’t have Foundation yet.)
   #if !os(WASI)
-    // #workaround(workspace version 0.34.0, The generated Xcode project cannot import XCTest on iOS devices.)
-    #if !Xcode || MANIFEST_LOADED_BY_XCODE || !(os(iOS) || os(tvOS)) || targetEnvironment(simulator)
-      import XCTest
+    import XCTest
 
-      import SDGLogic
-      import SDGPersistence
-      import SDGTesting
+    import SDGLogic
+    import SDGPersistence
+    import SDGTesting
 
-      /// A test case which simplifies testing for targets which link against the `SDGCornerstone` package.
-      open class TestCase: XCTestCase {
+    /// A test case which simplifies testing for targets which link against the `SDGCornerstone` package.
+    open class TestCase: XCTestCase {
 
-        private static var initialized = false
-        open override func setUp() {
-          if ¬TestCase.initialized {
-            TestCase.initialized = true
-            ProcessInfo.applicationIdentifier =
-              "ca.solideogloria.SDGCornerstone.SharedTestingDomain"
-          }
-
-          testAssertionMethod = XCTAssert
-
-          // The default of .userInteractive is absurd.
-          Thread.current.qualityOfService = .utility
-
-          super.setUp()
+      private static var initialized = false
+      open override func setUp() {
+        if ¬TestCase.initialized {
+          TestCase.initialized = true
+          ProcessInfo.applicationIdentifier =
+            "ca.solideogloria.SDGCornerstone.SharedTestingDomain"
         }
-      }
 
-    #endif
+        testAssertionMethod = XCTAssert
+
+        // The default of .userInteractive is absurd.
+        Thread.current.qualityOfService = .utility
+
+        super.setUp()
+      }
+    }
+
   #endif
 #endif
