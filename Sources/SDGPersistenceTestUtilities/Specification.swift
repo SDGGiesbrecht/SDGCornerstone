c246ccc30dee2a7794c22e12b17d97260ea62f01
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-09-21 20:27:57 -0600

    Fixed specification directory?

--- Sources/SDGPersistenceTestUtilities/Specification.swift
+++ Sources/SDGPersistenceTestUtilities/Specification.swift
@@ -59,7 +59,7 @@
         #if os(Windows)
           // Convert WSL paths to native Windows paths if cross‐compiled.
           var directory = callerURL.path
-          if directory.hasPrefix("\u{5C}mnt\u{5C}") {
+          if directory.hasPrefix("/mnt/") {
             directory.removeFirst(5)
             let driveLetter = directory.removeFirst()
             directory.prepend(contentsOf: "\(driveLetter.uppercased()):")
