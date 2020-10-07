eb7ba985fd6dbad3ab23c7cb7b5157376aa07dcf
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-10-07 17:49:39 -0600

    Updated watchOS workaround.

--- Package.swift
+++ Package.swift
@@ -752,7 +752,7 @@ if ProcessInfo.processInfo.environment["TARGETING_IOS"] == "true" {
 }
 
 if ProcessInfo.processInfo.environment["TARGETING_WATCHOS"] == "true" {
-  // #workaround(xcodebuild -version 11.6, Test targets don’t work on watchOS.) @exempt(from: unicode)
+  // #workaround(xcodebuild -version 12, Test targets don’t work on watchOS.) @exempt(from: unicode)
   package.targets.removeAll(where: { $0.isTest })
   // #workaround(xcodebuild -version 12, Tool targets don’t work on watchOS.) @exempt(from: unicode)
   package.targets.removeAll(where: { $0.name.hasPrefix("generate") })
