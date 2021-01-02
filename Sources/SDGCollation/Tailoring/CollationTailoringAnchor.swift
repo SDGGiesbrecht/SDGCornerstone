07c082da387e82d54d2d57bdd008b0b74c74c816
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2021-01-01 18:34:57 -0600

    Updated Workspace.

--- Sources/SDGCollation/Tailoring/CollationTailoringAnchor.swift
+++ Sources/SDGCollation/Tailoring/CollationTailoringAnchor.swift
@@ -4,7 +4,7 @@
  This source file is part of the SDGCornerstone open source project.
  https://sdggiesbrecht.github.io/SDGCornerstone
 
- Copyright ©2019–2020 Jeremy David Giesbrecht and the SDGCornerstone project contributors.
+ Copyright ©2019–2021 Jeremy David Giesbrecht and the SDGCornerstone project contributors.
 
  Soli Deo gloria.
 
