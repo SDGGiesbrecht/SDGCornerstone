f38d35cd7364f39cd09fc96b9b86e3350651fc51
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2021-01-01 21:27:46 -0600

    ...

--- Tests/SDGCornerstoneDocumentationExampleTests/ReadMeExampleTests.swift
+++ Tests/SDGCornerstoneDocumentationExampleTests/ReadMeExampleTests.swift
@@ -28,7 +28,9 @@ import SDGXCTestUtilities
 class ReadMeExampleTests: TestCase {
 
   func testReadMe() {
-    #if !os(Windows)  // #workaround(Swift 5.3.1, Segmentation fault.)
+    // #workaround(Swift 5.3.1, Segmentation fault.)
+    // #workaround(Swift 5.3.2, Web lacks FileManager.)
+    #if !(os(Windows) || os(WASI))
       LocalizationSetting(orderOfPrecedence: ["en"]).do {
         // @example(readMe🇨🇦EN)
         // ••••••• Localization •••••••
