0f74ac393b1293011aefa5f098ee3668e5d50076
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2021-01-17 23:13:40 -0600

    Extracted single value container.

--- Sources/SDGXML/Coding/XML.Encoder.Implementation.swift
+++ Sources/SDGXML/Coding/XML.Encoder.Implementation.swift
@@ -18,7 +18,7 @@ extension XML.Encoder {
 
   #warning("Temporarily disabled.")
   #if false
-    internal class Implementation: Encoder, SingleValueEncodingContainer {
+    internal class Implementation: Encoder {
       #warning("Is classhood necessary?")
 
       // MARK: - Initailzation
@@ -67,87 +67,6 @@ extension XML.Encoder {
 
       internal func singleValueContainer() -> SingleValueEncodingContainer {
         return self
-      }
-
-      // MARK: - SingleValueEncodingContainer
-
-      internal func encodeNil() throws {
-        #warning("Not implemented yet.")
-        fatalError()
-      }
-
-      internal func encode(_ value: Bool) throws {
-        #warning("Not implemented yet.")
-        fatalError()
-      }
-
-      internal func encode(_ value: String) throws {
-        partialNodes.append(box(value))
-      }
-
-      internal func encode(_ value: Double) throws {
-        #warning("Not implemented yet.")
-        fatalError()
-      }
-
-      internal func encode(_ value: Float) throws {
-        #warning("Not implemented yet.")
-        fatalError()
-      }
-
-      internal func encode(_ value: Int) throws {
-        #warning("Not implemented yet.")
-        fatalError()
-      }
-
-      internal func encode(_ value: Int8) throws {
-        #warning("Not implemented yet.")
-        fatalError()
-      }
-
-      internal func encode(_ value: Int16) throws {
-        #warning("Not implemented yet.")
-        fatalError()
-      }
-
-      internal func encode(_ value: Int32) throws {
-        #warning("Not implemented yet.")
-        fatalError()
-      }
-
-      internal func encode(_ value: Int64) throws {
-        #warning("Not implemented yet.")
-        fatalError()
-      }
-
-      internal func encode(_ value: UInt) throws {
-        #warning("Not implemented yet.")
-        fatalError()
-      }
-
-      internal func encode(_ value: UInt8) throws {
-        #warning("Not implemented yet.")
-        fatalError()
-      }
-
-      internal func encode(_ value: UInt16) throws {
-        #warning("Not implemented yet.")
-        fatalError()
-      }
-
-      internal func encode(_ value: UInt32) throws {
-        #warning("Not implemented yet.")
-        fatalError()
-      }
-
-      internal func encode(_ value: UInt64) throws {
-        #warning("Not implemented yet.")
-        fatalError()
-      }
-
-      internal func encode<T>(_ value: T) throws where T: Encodable {
-        #warning("Not implemented yet.")
-        fatalError()
       }
     }
   #endif
