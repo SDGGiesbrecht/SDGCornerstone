4903068eb97c443b708ba41d78dc17251543b10c
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2021-01-18 17:35:29 -0600

    Restored tests.

--- Tests/SDGXMLTests/APITests.swift
+++ Tests/SDGXMLTests/APITests.swift
@@ -109,51 +109,45 @@ class APITests: TestCase {
   }
 
   func testXMLEncoder() throws {
-    let encoder = XML.Encoder.Implementation(rootElementName: "root", userInfo: [:])
-    try true.encode(to: encoder)
-    print(encoder.element.source())
-    #warning("Temporarily disabled.")
-    #if false
-      let specifications = testSpecificationDirectory().appendingPathComponent("Codable XML")
+    let specifications = testSpecificationDirectory().appendingPathComponent("Codable XML")
 
-      func testXML<Value: Encodable>(
-        of value: Value,
-        specification: StrictString,
-        overwriteSpecificationInsteadOfFailing: Bool,
-        file: StaticString = #filePath,
-        line: UInt = #line
-      ) throws {
-        let encoder = XML.Encoder()
-        let xml: Data = try encoder.encode(value)
-        let source = try StrictString(file: xml, origin: nil)
-        compare(
-          String(source),
-          against: specifications.appendingPathComponent("\(specification).txt"),
-          overwriteSpecificationInsteadOfFailing: overwriteSpecificationInsteadOfFailing,
-          file: file,
-          line: line
-        )
-      }
+    func testXML<Value: Encodable>(
+      of value: Value,
+      specification: StrictString,
+      overwriteSpecificationInsteadOfFailing: Bool,
+      file: StaticString = #filePath,
+      line: UInt = #line
+    ) throws {
+      let encoder = XML.Encoder()
+      let xml: Data = try encoder.encode(value)
+      let source = try StrictString(file: xml, origin: nil)
+      compare(
+        String(source),
+        against: specifications.appendingPathComponent("\(specification).txt"),
+        overwriteSpecificationInsteadOfFailing: overwriteSpecificationInsteadOfFailing,
+        file: file,
+        line: line
+      )
+    }
 
-      try testXML(
-        of: "string",
-        specification: "String",
-        overwriteSpecificationInsteadOfFailing: false
-      )
-      try testXML(
-        of: [
-          "key": "value",
-          "Schlüssel": "Wert",
-          "clef": "valeur",
-        ],
-        specification: "Dictionary",
-        overwriteSpecificationInsteadOfFailing: false
-      )
-      try testXML(
-        of: ["A", "B", "C"],
-        specification: "Dictionary",
-        overwriteSpecificationInsteadOfFailing: false
-      )
-    #endif
+    try testXML(
+      of: "string",
+      specification: "String",
+      overwriteSpecificationInsteadOfFailing: false
+    )
+    try testXML(
+      of: [
+        "key": "value",
+        "Schlüssel": "Wert",
+        "clef": "valeur",
+      ],
+      specification: "Dictionary",
+      overwriteSpecificationInsteadOfFailing: false
+    )
+    try testXML(
+      of: ["A", "B", "C"],
+      specification: "Dictionary",
+      overwriteSpecificationInsteadOfFailing: false
+    )
   }
 }
