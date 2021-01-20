96a50b1ddd0740f7f684147c0b0437202c339a08
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2021-01-19 19:33:00 -0600

    Removed redundant methods.

--- Sources/SDGXML/Coding/XML.Decoder.KeyedContainer.swift
+++ Sources/SDGXML/Coding/XML.Decoder.KeyedContainer.swift
@@ -68,18 +68,6 @@ extension XML.Decoder {
       return value
     }
 
-    #warning("Should this check the type, or just return nil?")
-    private func decodeFromLosslessStringIfPresent<T>(_ type: T.Type, forKey key: Key) throws -> T?
-    where T: LosslessStringConvertible {
-      guard let string = try decodeIfPresent(String.self, forKey: key) else {
-        return nil
-      }
-      guard let value = T(string) else {
-        throw mismatchedTypeError(T.self, codingPath: decoder.codingPath.appending(key))
-      }
-      return value
-    }
-
     // MARK: - KeyedEncodingContainerProtocol
 
     internal var allKeys: [Key] {
@@ -163,74 +151,6 @@ extension XML.Decoder {
       return try decoder.enterElement(key: key) { element in
         return try T(from: decoder)
       }
-    }
-
-    #warning("Are these provided?")
-    internal func decodeIfPresent(_ type: Bool.Type, forKey key: Key) throws -> Bool? {
-      return try decodeFromLosslessStringIfPresent(Bool.self, forKey: key)
-    }
-
-    internal func decodeIfPresent(_ type: Int.Type, forKey key: Key) throws -> Int? {
-      return try decodeFromLosslessStringIfPresent(Int.self, forKey: key)
-    }
-
-    internal func decodeIfPresent(_ type: Int8.Type, forKey key: Key) throws -> Int8? {
-      return try decodeFromLosslessStringIfPresent(Int8.self, forKey: key)
-    }
-
-    internal func decodeIfPresent(_ type: Int16.Type, forKey key: Key) throws -> Int16? {
-      return try decodeFromLosslessStringIfPresent(Int16.self, forKey: key)
-    }
-
-    internal func decodeIfPresent(_ type: Int32.Type, forKey key: Key) throws -> Int32? {
-      return try decodeFromLosslessStringIfPresent(Int32.self, forKey: key)
-    }
-
-    internal func decodeIfPresent(_ type: Int64.Type, forKey key: Key) throws -> Int64? {
-      return try decodeFromLosslessStringIfPresent(Int64.self, forKey: key)
-    }
-
-    internal func decodeIfPresent(_ type: UInt.Type, forKey key: Key) throws -> UInt? {
-      return try decodeFromLosslessStringIfPresent(UInt.self, forKey: key)
-    }
-
-    internal func decodeIfPresent(_ type: UInt8.Type, forKey key: Key) throws -> UInt8? {
-      return try decodeFromLosslessStringIfPresent(UInt8.self, forKey: key)
-    }
-
-    internal func decodeIfPresent(_ type: UInt16.Type, forKey key: Key) throws -> UInt16? {
-      return try decodeFromLosslessStringIfPresent(UInt16.self, forKey: key)
-    }
-
-    internal func decodeIfPresent(_ type: UInt32.Type, forKey key: Key) throws -> UInt32? {
-      return try decodeFromLosslessStringIfPresent(UInt32.self, forKey: key)
-    }
-
-    internal func decodeIfPresent(_ type: UInt64.Type, forKey key: Key) throws -> UInt64? {
-      return try decodeFromLosslessStringIfPresent(UInt64.self, forKey: key)
-    }
-
-    internal func decodeIfPresent(_ type: Double.Type, forKey key: Key) throws -> Double? {
-      return try decodeFromLosslessStringIfPresent(Double.self, forKey: key)
-    }
-
-    internal func decodeIfPresent(_ type: Float.Type, forKey key: Key) throws -> Float? {
-      return try decodeFromLosslessStringIfPresent(Float.self, forKey: key)
-    }
-
-    internal func decodeIfPresent(_ type: String.Type, forKey key: Key) throws -> String? {
-      guard contains(key) else {
-        return nil
-      }
-      return try decode(type, forKey: key)
-    }
-
-    internal func decodeIfPresent<T>(_ type: T.Type, forKey key: Key) throws -> T?
-    where T: Decodable {
-      guard contains(key) else {
-        return nil
-      }
-      return try decode(type, forKey: key)
     }
 
     internal func nestedContainer<NestedKey>(
