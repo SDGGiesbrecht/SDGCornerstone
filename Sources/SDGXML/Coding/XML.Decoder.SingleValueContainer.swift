5b170497c30b974eb70c3ece1a10e15c9c671f78
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2021-01-19 22:40:29 -0600

    Implemented singe value.

---
+++ Sources/SDGXML/Coding/XML.Decoder.SingleValueContainer.swift
@@ -0,0 +1,54 @@
+/*
+ XML.Decoder.SingleValueContainer.swift
+
+ This source file is part of the SDGCornerstone open source project.
+ https://sdggiesbrecht.github.io/SDGCornerstone
+
+ Copyright ©2021 Jeremy David Giesbrecht and the SDGCornerstone project contributors.
+
+ Soli Deo gloria.
+
+ Licensed under the Apache Licence, Version 2.0.
+ See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
+ */
+
+import SDGText
+
+extension XML.Decoder {
+
+  internal struct SingleValueContainer: SingleValueDecodingContainer, XMLDecoderKeylessContainer {
+
+    // MARK: - Initialization
+
+    internal init(decoder: XML.Decoder.Implementation) {
+      self.decoder = decoder
+    }
+
+    // MARK: - Properties
+
+    internal let decoder: XML.Decoder.Implementation
+
+    // MARK: - SingleValueDecodingContainer
+
+    internal func decodeNil() -> Bool {
+      return decoder.currentElement.name == StrictString(XML.Coder.MiscellaneousKey.nil)
+    }
+
+    internal func decode(_ type: String.Type) throws -> String {
+      guard let text = decoder.currentElement.data else {
+        throw decoder.mismatchedTypeError(String.self, codingPath: codingPath)
+      }
+      return String(text)
+    }
+
+    internal func decode<T>(_ type: T.Type) throws -> T where T: Decodable {
+      return try T(from: decoder)
+    }
+
+    // MARK: - XMLDecoderKeylessContainer
+
+    internal var indexKey: CodingKey? {
+      return nil
+    }
+  }
+}
