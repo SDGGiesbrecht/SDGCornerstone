901f66fe617fb1463dbb46799d1c3965e9222aee
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2021-02-12 17:48:08 -0600

    Implemented maps for closed ranges.

--- Sources/SDGCollections/Ordered/Range.swift
+++ Sources/SDGCollections/Ordered/Range.swift
@@ -89,6 +89,34 @@ extension Range: RangeFamily {
 
 extension ClosedRange: RangeFamily {
 
+  /// Maps the range to another bound type.
+  ///
+  /// - Precondition: The conversion must generate valid bounds—the upper bound must not precede the lower bound.
+  ///
+  /// - Parameters:
+  ///     - convert: A closure which converts a single bound.
+  ///     - bound: The bound to convert.
+  @inlinable public func map<B>(_ convert: (_ bound: Bound) -> B) -> ClosedRange<B> {
+    return convert(lowerBound)...convert(upperBound)
+  }
+
+  /// Maps the range to another bound type.
+  ///
+  /// The result will be `nil` if either bound conversion fails, or if the upper bound ends up preceding the lower bound.
+  ///
+  /// - Parameters:
+  ///     - convert: A closure which converts a single bound.
+  ///     - bound: The bound to convert.
+  @inlinable public func map<B>(_ convert: (_ bound: Bound) -> B?) -> ClosedRange<B>? {
+    guard let lower = convert(lowerBound),
+      let upper = convert(upperBound),
+      lower ≤ upper
+    else {
+      return nil
+    }
+    return lower...upper
+  }
+
   // MARK: - RangeFamily
 
   /// `true` if the type has a closed upper bound.
