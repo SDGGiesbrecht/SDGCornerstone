f39d6cef45c3e8aea96fde6a62fa184fe659d008
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2021-09-04 16:09:50 -0600

    Refreshed.

--- Sources/SDGXCTestUtilities/TestCase.swift
+++ Sources/SDGXCTestUtilities/TestCase.swift
@@ -12,32 +12,32 @@
  See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
  */
 
-  import XCTest
+import XCTest
 
-  import SDGLogic
-  import SDGPersistence
-  import SDGTesting
+import SDGLogic
+import SDGPersistence
+import SDGTesting
 
-  /// A test case which simplifies testing for targets which link against the `SDGCornerstone` package.
-  open class TestCase: XCTestCase {
+/// A test case which simplifies testing for targets which link against the `SDGCornerstone` package.
+open class TestCase: XCTestCase {
 
-    private static var initialized = false
-    open override func setUp() {
-      if ¬TestCase.initialized {
-        TestCase.initialized = true
-        #if !PLATFORM_LACKS_FOUNDATION_PROCESS_INFO
-          ProcessInfo.applicationIdentifier =
-            "ca.solideogloria.SDGCornerstone.SharedTestingDomain"
-        #endif
-      }
-
-      testAssertionMethod = XCTAssert
-
-      #if !PLATFORM_LACKS_FOUNDATION_THREAD
-        // The default of .userInteractive is absurd.
-        Thread.current.qualityOfService = .utility
+  private static var initialized = false
+  open override func setUp() {
+    if ¬TestCase.initialized {
+      TestCase.initialized = true
+      #if !PLATFORM_LACKS_FOUNDATION_PROCESS_INFO
+        ProcessInfo.applicationIdentifier =
+          "ca.solideogloria.SDGCornerstone.SharedTestingDomain"
       #endif
-
-      super.setUp()
     }
+
+    testAssertionMethod = XCTAssert
+
+    #if !PLATFORM_LACKS_FOUNDATION_THREAD
+      // The default of .userInteractive is absurd.
+      Thread.current.qualityOfService = .utility
+    #endif
+
+    super.setUp()
   }
+}
--- Sources/SDGXCTestUtilities/XCTAssert.swift
+++ Sources/SDGXCTestUtilities/XCTAssert.swift
@@ -12,33 +12,33 @@
  See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
  */
 
-  import XCTest
+import XCTest
 
-  import SDGLogic
-  import SDGMathematics
-  import SDGText
+import SDGLogic
+import SDGMathematics
+import SDGText
 
-  // @localization(🇩🇪DE) @notLocalized(🇨🇦EN)
-  /// Stellt fest, ob zwei Werte gleich sind.
-  ///
-  /// - Parameters:
-  ///     - ausdruck1: Der erste Ausdruck.
-  ///     - ausdruck2: Der zweite Ausdruck.
-  ///     - mitteilung: Eine beschreibung des Fehlers.
-  ///     - datei: Die Datei.
-  ///     - zeile: Die Zeile.
-  @inlinable public func XCTFeststellenGleich<T>(
-    _ ausdruck1: @autoclosure () throws -> T,
-    _ ausdruck2: @autoclosure () throws -> T,
-    _ mitteilung: @autoclosure () -> Zeichenkette = "",  // @exempt(from: tests)
-    datei: StatischeZeichenkette = #filePath,
-    zeile: NZahl = #line
-  ) where T: Vergleichbar {  // @exempt(from: tests)
-    XCTAssertEqual(
-      try ausdruck1(),  // @exempt(from: tests)
-      try ausdruck2(),  // @exempt(from: tests)
-      mitteilung(),  // @exempt(from: tests)
-      file: datei,
-      line: zeile
-    )
-  }
+// @localization(🇩🇪DE) @notLocalized(🇨🇦EN)
+/// Stellt fest, ob zwei Werte gleich sind.
+///
+/// - Parameters:
+///     - ausdruck1: Der erste Ausdruck.
+///     - ausdruck2: Der zweite Ausdruck.
+///     - mitteilung: Eine beschreibung des Fehlers.
+///     - datei: Die Datei.
+///     - zeile: Die Zeile.
+@inlinable public func XCTFeststellenGleich<T>(
+  _ ausdruck1: @autoclosure () throws -> T,
+  _ ausdruck2: @autoclosure () throws -> T,
+  _ mitteilung: @autoclosure () -> Zeichenkette = "",  // @exempt(from: tests)
+  datei: StatischeZeichenkette = #filePath,
+  zeile: NZahl = #line
+) where T: Vergleichbar {  // @exempt(from: tests)
+  XCTAssertEqual(
+    try ausdruck1(),  // @exempt(from: tests)
+    try ausdruck2(),  // @exempt(from: tests)
+    mitteilung(),  // @exempt(from: tests)
+    file: datei,
+    line: zeile
+  )
+}
