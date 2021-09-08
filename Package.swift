4a3440ecb4b9598584b9fa8e0fe868eb7570802e
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2021-09-08 14:34:21 -0600

    Updated tools version.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.4
 
 /*
  Package.swift
@@ -585,7 +585,7 @@ let package = Package(
 
     // Internal utilities.
 
-    .target(
+    .executableTarget(
       name: "generate‐root‐collation",
       dependencies: [
         "SDGLogic",
