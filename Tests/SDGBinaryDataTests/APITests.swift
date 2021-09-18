b24cac15747c69e3fa2023ada529d7a939dea6ea
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2021-09-17 22:00:39 -0600

    ...

--- Tests/SDGBinaryDataTests/APITests.swift
+++ Tests/SDGBinaryDataTests/APITests.swift
@@ -41,9 +41,9 @@ class APITests: TestCase {
 
       #warning("Debugging...")
       print("B")
+      let data = Data([UInt8.max])
     #warning("Debugging...")
     #if !PLATFORM_SUFFERS_SEGMENTATION_FAULTS
-      let data = Data([UInt8.max])
       XCTAssertEqual(data.binary.count, 8)
       XCTAssertEqual(data.binary.map({ $0 ? "1" : "0" }).joined(), "11111111")
     #endif
