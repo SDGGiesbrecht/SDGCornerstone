c4bc5fe958ab913f9374d7cec518d3b57f04796f
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2021-11-30 14:49:36 -0600

    Fixed condition that was missed.

--- Sources/SDGMathematics/Float.swift
+++ Sources/SDGMathematics/Float.swift
@@ -19,7 +19,7 @@ import Foundation
 
 import RealModule
 
-#if PLATFORM_LACKS_SWIFT_FLOAT_80 || (os(macOS) && arch(arm64))
+#if PLATFORM_LACKS_SWIFT_FLOAT_80 || ((os(macOS) || os(Linux)) && arch(arm64))
   // #documentation(FloatMax)
   /// The member of the `Float` family with the largest bit field.
   public typealias FloatMax = Double
