a372e83b47456990b3c84b54956a449064004019
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2021-12-12 23:30:43 -0600

    Updated.

--- Tests/SDGBinaryDataTests/APITests.swift
+++ Tests/SDGBinaryDataTests/APITests.swift
@@ -39,7 +39,9 @@ class APITests: TestCase {
 
     let data = Data([UInt8.max])
     XCTAssertEqual(data.binary.count, 8)
-    XCTAssertEqual(data.binary.map({ $0 ? "1" : "0" }).joined(), "11111111")
+    #if !os(Windows)  // #workaround(Swift 5.5.1, Compiler trips over bitshift in UInt BinaryView.)
+      XCTAssertEqual(data.binary.map({ $0 ? "1" : "0" }).joined(), "11111111")
+    #endif
 
     #if !PLATFORM_SUFFERS_SEGMENTATION_FAULTS
       var toReverse = Data([0b11110000, 0b00000000])
