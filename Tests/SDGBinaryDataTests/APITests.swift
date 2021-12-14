5de1d3cfd79862dd761f91907aec9c062d7ba8d7
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2021-12-13 23:05:28 -0600

    Refreshed.

--- Tests/SDGBinaryDataTests/APITests.swift
+++ Tests/SDGBinaryDataTests/APITests.swift
@@ -43,21 +43,21 @@ class APITests: TestCase {
       XCTAssertEqual(data.binary.map({ $0 ? "1" : "0" }).joined(), "11111111")
     #endif
 
-      var toReverse = Data([0b11110000, 0b00000000])
+    var toReverse = Data([0b11110000, 0b00000000])
     #if !os(Windows)  // #workaround(Swift 5.5.1, Compiler trips over bitshift in UInt BinaryView subscript.)
       toReverse.binary.reverse()
       XCTAssertEqual(toReverse, Data([0b000000000, 0b00001111]))
     #endif
 
-      let alternating = Data([0b01010101, 0b01010101])
-      let sorted = Data([0b00000000, 0b11111111])
+    let alternating = Data([0b01010101, 0b01010101])
+    let sorted = Data([0b00000000, 0b11111111])
 
-      XCTAssertEqual(alternating.bitwiseNot(), Data([0b10101010, 0b10101010]))
-      XCTAssertEqual(alternating.bitwiseAnd(with: sorted), Data([0b00000000, 0b01010101]))
-      XCTAssertEqual(alternating.bitwiseOr(with: sorted), Data([0b01010101, 0b11111111]))
-      XCTAssertEqual(alternating.bitwiseExclusiveOr(with: sorted), Data([0b01010101, 0b10101010]))
-    
-      var forDescription = Data([0, 0])
+    XCTAssertEqual(alternating.bitwiseNot(), Data([0b10101010, 0b10101010]))
+    XCTAssertEqual(alternating.bitwiseAnd(with: sorted), Data([0b00000000, 0b01010101]))
+    XCTAssertEqual(alternating.bitwiseOr(with: sorted), Data([0b01010101, 0b11111111]))
+    XCTAssertEqual(alternating.bitwiseExclusiveOr(with: sorted), Data([0b01010101, 0b10101010]))
+
+    var forDescription = Data([0, 0])
     #if !os(Windows)  // #workaround(Swift 5.5.1, Compiler trips over bitshift in UInt BinaryView subscript.)
       forDescription.binary[11] = true
       testCustomStringConvertibleConformance(
