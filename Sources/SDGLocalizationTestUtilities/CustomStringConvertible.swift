825bd453d6250a99b58471d3a42f8395fddcbd35
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2022-01-05 16:28:22 -0600

    ...

--- Sources/SDGLocalizationTestUtilities/CustomStringConvertible.swift
+++ Sources/SDGLocalizationTestUtilities/CustomStringConvertible.swift
@@ -44,12 +44,12 @@ public func testCustomStringConvertibleConformance<T, L>(
 
   var report = ""
   for localization in localizations.allCases {
-    #if false
     if let icon = localization.icon {
       report.append(contentsOf: String(icon))
     } else {
       report.append(contentsOf: localization.code)
     }
+    #if false
     report.append("\n")
     LocalizationSetting(orderOfPrecedence: [localization.code]).do {
       report.append(contentsOf: String(describing: instance))
