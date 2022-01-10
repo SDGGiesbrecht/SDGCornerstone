e8eee72e1dcd3ca5f83ce110807334f8509c724f
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2022-01-09 23:12:06 -0600

    ...

--- Sources/SDGCalendar/Gregorian/GregorianDay.swift
+++ Sources/SDGCalendar/Gregorian/GregorianDay.swift
@@ -45,6 +45,11 @@ public struct GregorianDay: CodableViaRawRepresentableCalendarComponent,
   // MARK: - CodableViaRawRepresentableCalendarComponent
 
   // #workaround(Swift 5.5.2, Redundant, but evades Windows compiler bug.)
+  public func encode(to encoder: Encoder) throws {
+    try encode(to: encoder, via: rawValue)
+  }
+
+  // #workaround(Swift 5.5.2, Redundant, but evades Windows compiler bug.)
   public init(from decoder: Decoder) throws {
     try self.init(from: decoder, via: RawValue.self, convert: { Self(possibleRawValue: $0) })
   }
