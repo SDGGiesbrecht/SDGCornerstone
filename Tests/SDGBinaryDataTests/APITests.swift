daea8a0b7687a96313ff6b84395cbd305b5ebcaf
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2022-01-12 22:11:32 -0600

    ...

--- Tests/SDGBinaryDataTests/APITests.swift
+++ Tests/SDGBinaryDataTests/APITests.swift
@@ -88,7 +88,7 @@ class APITests: TestCase {
   func testUInt() {
     var forDescription: UInt8 = 0
     #warning("Debugging...")
-    let int: UInt.Vector = 0
+    let int: UInt.Vector = .zero
     int.verifyAddable()
     int.verifyAdditiveArithmetic()
     int.verifyDecodable()
