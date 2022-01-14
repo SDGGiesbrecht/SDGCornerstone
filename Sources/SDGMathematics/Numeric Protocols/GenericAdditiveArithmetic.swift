38d332d3b233a3e636aed08beb70ca9c55650abe
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2022-01-14 00:43:57 -0600

    ...

--- Sources/SDGMathematics/Numeric Protocols/GenericAdditiveArithmetic.swift
+++ Sources/SDGMathematics/Numeric Protocols/GenericAdditiveArithmetic.swift
@@ -15,8 +15,8 @@
 /// A value that can be added and subtracted.
 ///
 /// `GenericAdditiveArithmetic` types do not need to conform to `Comparable`, allowing conformance by two‐dimensional vectors, etc. For additional behaviour specific to one‐dimensional types, see `NumericAdditiveArithmetic`.
-public protocol GenericAdditiveArithmetic: AdditiveArithmetic, Decodable, Encodable, Hashable,
-  Addable/*Subtractable*/
+public protocol GenericAdditiveArithmetic: AdditiveArithmetic, Decodable, Encodable, Hashable/*,
+  Addable*//*Subtractable*/
 {}
 
 extension GenericAdditiveArithmetic {
@@ -27,7 +27,7 @@ extension GenericAdditiveArithmetic {
     verifyDecodable()
     verifyEncodable()
     verifyHashable()
-    verifyAddable()
+    //verifyAddable()
     //verifySubtractable()
     print(#function, Self.self)
   }
