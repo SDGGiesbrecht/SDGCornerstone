5786736993ed677677016f43a52fc6e2744f9b93
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2022-01-14 18:14:46 -0600

    Removed extra requirement.

--- Sources/Debugging/Numeric Protocols/GenericAdditiveArithmetic.swift
+++ Sources/Debugging/Numeric Protocols/GenericAdditiveArithmetic.swift
@@ -3,9 +3,7 @@
 /// `GenericAdditiveArithmetic` types do not need to conform to `Comparable`, allowing conformance by two‐dimensional vectors, etc. For additional behaviour specific to one‐dimensional types, see `NumericAdditiveArithmetic`.
 public protocol GenericAdditiveArithmetic: AdditiveArithmetic, Decodable, Encodable, Hashable,
   Subtractable
-{
-  func verifyGenericAdditiveArithmetic()
-}
+{}
 
 extension GenericAdditiveArithmetic {
   public func verifyGenericAdditiveArithmetic() {
