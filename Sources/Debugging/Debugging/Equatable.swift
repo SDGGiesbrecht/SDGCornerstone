418b314ca54b05bd886e813a960081f375dcf910
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2022-01-14 18:49:50 -0600

    ...

--- Sources/Debugging/Debugging/Equatable.swift
+++
@@ -1,7 +0,0 @@
-extension Equatable {
-
-  public func verifyEquatable() {
-    _ = self != self
-    print(#function)
-  }
-}
--- Sources/Debugging/Debugging/Hashable.swift
+++ Sources/Debugging/Debugging/Hashable.swift
@@ -1,7 +1,6 @@
 extension Hashable {
 
   public func verifyHashable() {
-    verifyEquatable()
     var hasher = Hasher()
     hash(into: &hasher)
     print(#function)
