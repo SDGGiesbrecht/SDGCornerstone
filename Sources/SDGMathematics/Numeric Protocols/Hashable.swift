e74f62cd6348c5c3894c241b8764fc125f534763
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2022-02-20 17:14:23 -0600

    Tried disambiguating.

--- Sources/SDGMathematics/Numeric Protocols/Hashable.swift
+++ Sources/SDGMathematics/Numeric Protocols/Hashable.swift
@@ -1,7 +1,7 @@
 
 // #workaround(Swift 5.5.3, Redundant, but evades SR‐15734.)
 public protocol _Hashable: Hashable, __Hashable {
-  //func hash(into hasher: inout Hasher)
+  func hash(into hasher: inout Hasher)
 }
 public protocol __Hashable {
   func hash(into hasher: inout Hasher)
