26ff8f8100c8993562abffc64bd5d1a352631902
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2022-02-20 16:25:16 -0600

    Reverted.

--- Sources/SDGLocalization/Extensions/Numbers/WholeArithmetic.swift
+++ Sources/SDGLocalization/Extensions/Numbers/WholeArithmetic.swift
@@ -29,7 +29,10 @@ extension WholeArithmetic {
   internal func mapping(for digits: [UnicodeScalar]) -> [Self: UnicodeScalar] {
     var result: [Self: UnicodeScalar] = [:]
     for value in digits.indices {
+#warning("Debugging...")
+#if false
       result[Self(UInt(value))] = digits[value]
+      #endif
     }
     return result
   }
