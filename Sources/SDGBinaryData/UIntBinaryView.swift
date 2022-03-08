d83a90f3c29ae070f39141fe2383245bcf3971d1
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2022-03-08 17:36:38 -0600

    Tried first.

--- Sources/SDGBinaryData/UIntBinaryView.swift
+++ Sources/SDGBinaryData/UIntBinaryView.swift
@@ -102,9 +102,10 @@ public struct BinaryView<UIntValue: UIntFamily>: BidirectionalCollection, Collec
     let bits = self.lazy.map { bit in
       return bit ? "1" : "0"
     }
-    for index in bits.indices {
+    let first = bits.startIndex
+    /*for index in bits.indices {
       _ = index
-    }
+    }*/
     return ""
     //return bits.joined()
   }
