46a9e74ad3132750db6d82b4c1e2de308928199b
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2022-03-07 23:39:14 -0600

    ...

--- Tests/SDGBinaryDataTests/APITests.swift
+++ Tests/SDGBinaryDataTests/APITests.swift
@@ -105,7 +105,7 @@ class APITests: TestCase {
   }
 }
 
-protocol IProtocol: CustomReflectable, CVarArg, FixedWidthInteger, GenericAdditiveArithmetic, WholeArithmetic, MirrorPath, SignedInteger {}
+protocol IProtocol: CustomReflectable, CVarArg, FixedWidthInteger, WholeArithmetic, MirrorPath, SignedInteger {}
 extension Int: IProtocol {}
 struct Wrapper<I> where I: IProtocol {
   init(_ i: I) {
