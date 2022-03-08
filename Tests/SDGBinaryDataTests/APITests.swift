be289a5de0ee27f07f1946d2bb788809d17398eb
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2022-03-07 23:40:15 -0600

    Tried without SignedInteger.

--- Tests/SDGBinaryDataTests/APITests.swift
+++ Tests/SDGBinaryDataTests/APITests.swift
@@ -105,7 +105,7 @@ class APITests: TestCase {
   }
 }
 
-protocol IProtocol: CustomReflectable, CVarArg, FixedWidthInteger, WholeArithmetic, SignedInteger {}
+protocol IProtocol: CustomReflectable, CVarArg, FixedWidthInteger, WholeArithmetic, SignedNumeric {}
 extension Int: IProtocol {}
 struct Wrapper<I> where I: IProtocol {
   init(_ i: I) {
