e09864a5d71a5f1e0d61c39969ec704e157fd88e
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2022-06-27 15:39:34 -0600

    Created test.

--- Tests/SDGCollectionsTests/ScratchSpace.swift
+++ Tests/SDGCollectionsTests/ScratchSpace.swift
@@ -1,8 +1,20 @@
 #warning("Removed this file.")
 
+import SDGCollections
+
 import XCTest
 
 final class ScratchSpaceTests: XCTestCase {
-  
-  func testExample() {}
+
+  func testPatternMatches() {
+    let string = "Hello!"
+    let matches = string.matches(in: string, at: string.startIndex)
+    XCTAssert(matches.first?.contents.elementsEqual(string) == true)
+  }
+
+  func testSearchableCollectionFirstMatch() {
+    let pattern = "!"
+    let firstMatch = "Hello!".firstMatch(for: pattern)
+    XCTAssert(firstMatch?.contents.elementsEqual(pattern) == true)
+  }
 }
