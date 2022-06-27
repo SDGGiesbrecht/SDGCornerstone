8141ec0f10470c43a82a200a6cf3e38e6c7412b4
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2022-06-27 16:53:47 -0600

    Added empty SearchableCollection.

---
+++ Sources/SDGCollections2/Ordered/Collection.swift
@@ -0,0 +1,23 @@
+/*
+ Collection.swift
+
+ This source file is part of the SDGCornerstone open source project.
+ https://sdggiesbrecht.github.io/SDGCornerstone
+
+ Copyright ©2022 Jeremy David Giesbrecht and the SDGCornerstone project contributors.
+
+ Soli Deo gloria.
+
+ Licensed under the Apache Licence, Version 2.0.
+ See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
+ */
+
+extension Collection {
+
+  // MARK: - Indices
+
+  /// Returns the range for all of `self`.
+  @inlinable public var bounds: Range<Index> {
+    return startIndex..<endIndex
+  }
+}
