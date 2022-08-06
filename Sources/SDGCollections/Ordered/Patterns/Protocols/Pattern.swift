a5133ed5930d272dc5408d09696c2d695d24f081
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2022-08-05 22:45:39 -0600

    Merged Pattern.

--- Sources/SDGCollections/Ordered/Patterns/Protocols/Pattern.swift
+++ Sources/SDGCollections/Ordered/Patterns/Protocols/Pattern.swift
@@ -4,7 +4,7 @@
  This source file is part of the SDGCornerstone open source project.
  https://sdggiesbrecht.github.io/SDGCornerstone
 
- Copyright ©2022 Jeremy David Giesbrecht and the SDGCornerstone project contributors.
+ Copyright ©2018–2022 Jeremy David Giesbrecht and the SDGCornerstone project contributors.
 
  Soli Deo gloria.
 
