b31817787c1da05165a1ccac6a5a49271a9fc63a
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2022-08-08 22:21:40 -0600

    ...

--- Sources/SDGCollections/Ordered/RangeExpression.swift
+++ Sources/SDGCollections/Ordered/RangeExpression.swift
@@ -12,7 +12,6 @@
  See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
  */
 
-#warning("Audit.")
 extension RangeExpression where Self: SetDefinition {
 
   // #documentation(SDGCornerstone.ExpressionPattern.~=)
