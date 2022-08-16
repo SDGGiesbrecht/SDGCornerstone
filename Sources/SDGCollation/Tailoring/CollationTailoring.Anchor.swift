e0058bfa8a5fcc96f8f6e7b7de4e234676d6b706
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2022-08-15 20:47:03 -0600

    Refreshed.

--- Sources/SDGCollation/Tailoring/CollationTailoring.Anchor.swift
+++ Sources/SDGCollation/Tailoring/CollationTailoring.Anchor.swift
@@ -1,5 +1,5 @@
 /*
- CollationTailoringAnchor.swift
+ CollationTailoring.Anchor.swift
 
  This source file is part of the SDGCornerstone open source project.
  https://sdggiesbrecht.github.io/SDGCornerstone
