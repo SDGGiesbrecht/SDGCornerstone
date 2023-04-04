99d3ed38b6f19ca285de4fcb23879adc369bc585
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2023-04-03 19:20:35 -0600

    Updated copyright.

--- Sources/SDGLocalization/Extensions/Numbers/IntegralArithmetic.swift
+++ Sources/SDGLocalization/Extensions/Numbers/IntegralArithmetic.swift
@@ -4,7 +4,7 @@
  This source file is part of the SDGCornerstone open source project.
  https://sdggiesbrecht.github.io/SDGCornerstone
 
- Copyright ©2016–2022 Jeremy David Giesbrecht and the SDGCornerstone project contributors.
+ Copyright ©2016–2023 Jeremy David Giesbrecht and the SDGCornerstone project contributors.
 
  Soli Deo gloria.
 
