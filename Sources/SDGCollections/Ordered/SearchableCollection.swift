ea262db0e5cdec884bee650f2502401e3b0f3345
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2023-04-20 16:16:49 -0600

    Narrowed further.

--- Sources/SDGCollections/Ordered/SearchableCollection.swift
+++ Sources/SDGCollections/Ordered/SearchableCollection.swift
@@ -133,14 +133,13 @@ where Element: Equatable, Searchable == Self, SubSequence: SearchableCollection 
   ///
   /// - Parameters:
   ///   - pattern: The pattern to search for.
-  func contains<P>(_ pattern: P) -> Bool where P: Pattern, P.Searchable == Self
+  func contains<P>(_ pattern: P) -> Bool where P: Pattern, P.Searchable == Self*/
   // #documentation(SDGCornerstone.Collection.contains(pattern:))
   /// Returns `true` if `self` contains an match for `pattern`.
   ///
   /// - Parameters:
   ///   - pattern: The pattern to search for.
   func contains(_ pattern: Self) -> Bool
- */
 
   // @documentation(SDGCornerstone.Collection.hasPrefix(_:))
   /// Returns `true` if `self` begins with `pattern`.
