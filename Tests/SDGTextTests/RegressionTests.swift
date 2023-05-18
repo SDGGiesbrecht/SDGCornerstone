6ab9dc37860f6cab9d5b5131bf42ff28be099e2e
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2023-05-18 16:27:17 -0600

    Adjusted font test.

--- Tests/SDGTextTests/RegressionTests.swift
+++ Tests/SDGTextTests/RegressionTests.swift
@@ -89,11 +89,17 @@ class RegressionTests: TestCase {
         let attributedString = markup.richText(font: font)
         let attribute = attributedString.attribute(.font, at: 0, effectiveRange: nil)
         var originalName = font.fontName
-        if originalName == ".SFNSText" ∨ originalName == ".SFNS\u{2D}Regular" {
+        if originalName == ".SFNSText"
+          ∨ originalName == ".SFNS\u{2D}Regular"
+          ∨ originalName == ".SFCompact-Regular"
+          ∨ originalName == ".SFCompact-Black_opsz140000_GRAD_wght1900000" {
           originalName = ".AppleSystemUIFont"
         }
         var resultingName = (attribute as? NSFont)?.fontName
-        if resultingName == ".SFNSText" ∨ resultingName == ".SFNS\u{2D}Regular" {
+        if resultingName == ".SFNSText"
+          ∨ resultingName == ".SFNS\u{2D}Regular"
+          ∨ resultingName == ".SFCompact-Regular"
+          ∨ resultingName == ".SFCompact-Black_opsz140000_GRAD_wght1900000" {
           resultingName = ".AppleSystemUIFont"
         }
         XCTAssertEqual(resultingName, originalName)
