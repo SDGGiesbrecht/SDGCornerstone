0e23c14e31c2f11b86f0817284f047e714376635
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2023-05-18 18:50:40 -0600

    Fixed another test.

--- Tests/SDGTextTests/RegressionTests.swift
+++ Tests/SDGTextTests/RegressionTests.swift
@@ -91,15 +91,17 @@ class RegressionTests: TestCase {
         var originalName = font.fontName
         if originalName == ".SFNSText"
           ∨ originalName == ".SFNS\u{2D}Regular"
-          ∨ originalName == ".SFCompact-Regular"
-          ∨ originalName == ".SFCompact-Black_opsz140000_GRAD_wght1900000" {
+          ∨ originalName == ".SFCompact\u{2D}Regular"
+          ∨ originalName == ".SFCompact\u{2D}Black_opsz_GRAD_wght1900000"
+          ∨ originalName == ".SFCompact\u{2D}Black_opsz140000_GRAD_wght1900000" {
           originalName = ".AppleSystemUIFont"
         }
         var resultingName = (attribute as? NSFont)?.fontName
         if resultingName == ".SFNSText"
           ∨ resultingName == ".SFNS\u{2D}Regular"
-          ∨ resultingName == ".SFCompact-Regular"
-          ∨ resultingName == ".SFCompact-Black_opsz140000_GRAD_wght1900000" {
+          ∨ resultingName == ".SFCompact\u{2D}Regular"
+          ∨ resultingName == ".SFCompact\u{2D}Black_opsz_GRAD_wght1900000"
+          ∨ resultingName == ".SFCompact\u{2D}Black_opsz140000_GRAD_wght1900000" {
           resultingName = ".AppleSystemUIFont"
         }
         XCTAssertEqual(resultingName, originalName)
