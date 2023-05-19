d523c99554f7f65116ce4d3fe811c053a5bf5b65
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2023-05-19 15:20:33 -0600

    Updated more.

--- Package.swift
+++ Package.swift
@@ -903,7 +903,7 @@ for target in package.targets {
     // #workaround(Swift 5.8.0, Web lacks Foundation.UserDefaults.)
     // #workaround(Swift 5.8.0, Web lacks Foundation.PropertyListEncoder.)
     // #warning(Swift 5.7.1, FoundationXML is broken for web.)
-    // #warning(Swift 5.7.1, FoundationXML is broken on Android.)
+    // #workaround(Swift 5.8.0, FoundationXML is broken on Android.)
     // #workaround(Swift 5.7.2, macOS lacks Swift.Float16 for some architectures.)
     // @example(conditions)
     .define("PLATFORM_HAS_COCOA", .when(platforms: [.macOS, .tvOS, .iOS, .watchOS])),
@@ -915,7 +915,7 @@ for target in package.targets {
     .define("PLATFORM_LACKS_FOUNDATION_RUN_LOOP", .when(platforms: [.wasi])),
     .define("PLATFORM_LACKS_FOUNDATION_USER_DEFAULTS", .when(platforms: [.wasi])),
     .define("PLATFORM_LACKS_FOUNDATION_PROPERTY_LIST_ENCODER", .when(platforms: [.wasi])),
-    //.define("PLATFORM_LACKS_FOUNDATION_XML", .when(platforms: [.wasi, .android])),
+    .define("PLATFORM_LACKS_FOUNDATION_XML", .when(platforms: [/*.wasi,*/ .android])),
     .define(
       "PLATFORM_LACKS_FOUNDATION_XML_XML_DOCUMENT",
       .when(platforms: [/*.wasi,*/ .tvOS, .iOS, /*.android,*/ .watchOS])
@@ -939,10 +939,10 @@ for target in package.targets {
     .define("PLATFORM_LACKS_FOUNDATION_BUNDLE_BUNDLE_IDENTIFIER", .when(platforms: [.wasi])),
     // #workaround(Swift 5.8.0, Web lacks Foundation.Thread.)
     .define("PLATFORM_LACKS_FOUNDATION_THREAD", .when(platforms: [.wasi])),
-    // #warning(Swift 5.7.1, Android lacks FoundationNetworking.)
-    //.define("PLATFORM_LACKS_FOUNDATION_NETWORKING", .when(platforms: [.android])),
-    // #warning(Swift 5.7.1, Web lacks XCTest.XCTestExpectation.)
-    //.define("PLATFORM_LACKS_XC_TEST_XC_TEST_EXPECTATION", .when(platforms: [.wasi])),
+    // #workaround(Swift 5.8.0, Android lacks FoundationNetworking.)
+    .define("PLATFORM_LACKS_FOUNDATION_NETWORKING", .when(platforms: [.android])),
+    // #workaround(Swift 5.8.0, Web lacks XCTest.XCTestExpectation.)
+    .define("PLATFORM_LACKS_XC_TEST_XC_TEST_EXPECTATION", .when(platforms: [.wasi])),
     .define("PLATFORM_LACKS_GIT", .when(platforms: [.wasi, .tvOS, .iOS, .android, .watchOS])),
     .define(
       "PLATFORM_LACKS_SWIFT_COMPILER",
