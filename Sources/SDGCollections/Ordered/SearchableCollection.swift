df59b5aac65504b0282230c869e9a7c1b10d629a
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2023-05-22 18:18:35 -0600

    Removed web compiler workaround.

--- Sources/SDGCollections/Ordered/SearchableCollection.swift
+++ Sources/SDGCollections/Ordered/SearchableCollection.swift
@@ -128,14 +128,12 @@ where Element: Equatable, Searchable == Self, SubSequence: SearchableCollection 
   /// - Parameters:
   ///   - pattern: The pattern to search for.
   func contains<P>(_ pattern: P) -> Bool where P: Pattern, P.Searchable == Self
-  //#if !os(WASI)  // #warning(Swift 5.7.2, Trips the linker in Carton 0.18.)
-    // #documentation(SDGCornerstone.Collection.contains(pattern:))
-    /// Returns `true` if `self` contains an match for `pattern`.
-    ///
-    /// - Parameters:
-    ///   - pattern: The pattern to search for.
-    func contains(_ pattern: Self) -> Bool
-  //#endif
+  // #documentation(SDGCornerstone.Collection.contains(pattern:))
+  /// Returns `true` if `self` contains an match for `pattern`.
+  ///
+  /// - Parameters:
+  ///   - pattern: The pattern to search for.
+  func contains(_ pattern: Self) -> Bool
 
   // @documentation(SDGCornerstone.Collection.hasPrefix(_:))
   /// Returns `true` if `self` begins with `pattern`.
