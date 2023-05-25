eb656d19a6729f8066e66a6585ede927db306b12
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2023-05-24 22:50:32 -0600

    Updated instead.

--- Sources/SDGText/CollectionStringFamily.swift
+++ Sources/SDGText/CollectionStringFamily.swift
@@ -15,7 +15,7 @@
 import SDGMathematics
 import SDGCollections
 
-// #workaround(Swift 5.7.1, Should be “Element: StringFamily” but for Windows compiler crash.)
+// #workaround(Swift 5.8.0, Should be “Element: StringFamily” but for web compiler crash.)
 
 extension Collection where Element == StrictString {
 
